f913b3e16f5b4457963af9651780ceef9eb79d16
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-01-16 23:40:10 +0200

    Add files via upload

---
+++ Java/Programing Basics 2020 July-August/For Loop/Hospital.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Hospital {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int days = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+    }
+}
