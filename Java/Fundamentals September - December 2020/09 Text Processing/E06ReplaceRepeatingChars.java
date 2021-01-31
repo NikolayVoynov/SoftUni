082f8ebc5bc07a98fae0a2306d81572ed0be9478
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-01-31 10:59:42 +0200

    Add files via upload

---
+++ Java/Fundamentals September - December 2020/09 Text Processing/E06ReplaceRepeatingChars.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class E06ReplaceRepeatingChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        StringBuilder sb = new StringBuilder();
+
+        char previousChar = input.charAt(0);
+        sb.append(previousChar);
+
+        for (int i = 1; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+
+            if (currentChar != previousChar) {
+                sb.append(currentChar);
+                previousChar = currentChar;
+            }
+        }
+        System.out.println(sb);
+    }
+}
