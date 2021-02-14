2b1b75759beda4d9099faf678627d03dd2fd6c87
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-02-14 21:03:49 +0200

    Add files via upload

---
+++ Java/Advanced January - April 2021/01. Stacks & Queues/L06HotPotato.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class L06HotPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] kids = scanner.nextLine().split("\\s+");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<String> queue = new ArrayDeque<>();
+
+        for (String kid : kids) {
+            queue.offer(kid);
+        }
+
+        while (queue.size() > 1) {
+
+            for (int i = 1; i < n; i++) {
+                queue.offer(Objects.requireNonNull(queue.poll()));
+            }
+
+            System.out.println("Removed " + queue.poll());
+
+        }
+
+        System.out.println("Last is " + queue.poll());
+    }
+}
