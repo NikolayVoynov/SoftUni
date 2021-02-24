787ef4a8366f74bbb7e4a3651e2c8b1e7070d788
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-02-24 07:38:51 +0200

    Add files via upload

---
+++ Java/Advanced January - April 2021/04. Streams, Files & Directories/E01SumLines.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class E01SumLines {
+
+    private static final String Path = "C:\\Users\\user\\IdeaProjects\\AdvancedStreamsFilesDirectoriesExercise\\src\\Resources\\input.txt";
+
+    public static void main(String[] args) {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(Path))) {
+
+            String line = reader.readLine();
+
+            while (line != null) {
+                int sum = 0;
+
+                for (int i = 0; i < line.length(); i++) {
+                    sum += line.charAt(i);
+                }
+
+                System.out.println(sum);
+                line = reader.readLine();
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
