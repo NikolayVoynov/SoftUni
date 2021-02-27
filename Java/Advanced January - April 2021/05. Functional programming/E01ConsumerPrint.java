5ab1b8f69e5c5dd973086393d5caa059992b54fb
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-02-27 09:07:23 +0200

    Add files via upload

---
+++ Java/Advanced January - April 2021/05. Functional programming/E01ConsumerPrint.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class E01ConsumerPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Consumer<String> consumer = name -> System.out.println(name);
+
+        Arrays.stream(scanner.nextLine().split("\\s+")).forEach(consumer);
+    }
+}
