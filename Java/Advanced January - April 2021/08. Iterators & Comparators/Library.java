5b23b501c8cc772b81447c36e7c66968def82079
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-03-04 22:48:51 +0200

    Add files via upload

---
+++ Java/Advanced January - April 2021/08. Iterators & Comparators/Library.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+
+public class Library<Book> implements Iterable<Book> {
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    public void setBooks(Book[] books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new LibraryIterator();
+    }
+
+    private class LibraryIterator implements Iterator<Book> {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < books.length;
+        }
+
+        @Override
+        public Book next() {
+            return books[index++];
+        }
+    }
+
+
+}
