bb403dead3ef0bbb1f7ba02c7fdfaec4350b7912
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-03-21 08:59:40 +0200

    Add files via upload

---
+++ Java/OOP February - April 2021/04. Interfaces & Abstraction/L01CarShop/Car.java
@@ -0,0 +1,13 @@
+package L01CarShop;
+
+public interface Car {
+    Integer TIRES = 4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+}
