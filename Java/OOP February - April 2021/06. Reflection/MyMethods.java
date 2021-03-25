b83371164bb60a3c213d63487a4f17718b944586
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-03-25 22:50:25 +0200

    Add files via upload

---
+++ Java/OOP February - April 2021/06. Reflection/MyMethods.java
@@ -0,0 +1,17 @@
+public class MyMethods {
+
+    @Author(author = "Martin")
+    public void exampleOne() {
+
+    }
+
+    @Author(author = "A")
+    public void exampleTwo() {
+
+    }
+
+    @Author(author = "1")
+    public void exampleThree() {
+
+    }
+}
