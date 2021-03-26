c0ed66ae62770a0d61ad0fa38ec1d87ab4bf5e62
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-03-26 14:12:07 +0200

    Add files via upload

---
+++ Java/OOP February - April 2021/06. Reflection/barracksWars/core/CommandInterpreterImpl.java
@@ -0,0 +1,50 @@
+package barracksWars.core;
+
+import barracksWars.interfaces.*;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public class CommandInterpreterImpl implements CommandInterpreter {
+
+    private static final String COMMANDS_PATH = "barracksWars.core.commands.";
+
+    private Repository repository;
+    private UnitFactory unitFactory;
+
+    public CommandInterpreterImpl(Repository repository, UnitFactory unitFactory) {
+        this.repository = repository;
+        this.unitFactory = unitFactory;
+    }
+
+    @Override
+    public Executable interpretCommand(String[] data) {
+        Executable command = null;
+        String commandName = data[0];
+        commandName = Character.toUpperCase(commandName.charAt(0)) + commandName.substring(1);
+
+        try {
+            Class<?> clazz = Class.forName(COMMANDS_PATH + commandName);
+            Constructor<?> ctor = clazz.getDeclaredConstructor(String[].class);
+            ctor.setAccessible(true);
+            command = (Executable) ctor.newInstance(new Object[]{data});
+            Field[] executableFields = command.getClass().getDeclaredFields();
+            Field[] localFields = this.getClass().getDeclaredFields();
+            for (Field executableField : executableFields) {
+                if (executableField.isAnnotationPresent(Inject.class)) {
+                    for (Field localField : localFields) {
+                        if (executableField.getType().equals(localField.getType())) {
+                            executableField.setAccessible(true);
+                            executableField.set(command, localField.get(this));
+                        }
+                    }
+                }
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return command;
+    }
+}
---
+++ Java/OOP February - April 2021/06. Reflection/barracksWars/core/commands/Retire.java
@@ -0,0 +1,22 @@
+package barracksWars.core.commands;
+
+import barracksWars.interfaces.Inject;
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.UnitFactory;
+
+public class Retire extends Command {
+
+    @Inject
+    private Repository repository;
+
+    public Retire(String[] data) {
+        super(data);
+    }
+
+    @Override
+    public String execute() {
+        String unitType = getData()[1];
+        this.repository.removeUnit(unitType);
+        return unitType + " retired!";
+    }
+}
