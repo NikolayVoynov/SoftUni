ed59a2675c87b808d52a08bac8ea705dd720f6b5
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-04-01 17:43:31 +0300

    Add files via upload

--- Java/OOP February - April 2021/08. Test Driven Development/Test-Driven Development/src/main/java/Instock.java
+++ Java/OOP February - April 2021/08. Test Driven Development/Test-Driven Development/src/main/java/Instock.java
@@ -1,6 +1,7 @@
 import jdk.jshell.spi.ExecutionControl;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Instock implements ProductStock {
 
@@ -31,9 +32,7 @@ public class Instock implements ProductStock {
 
     @Override
     public void changeQuantity(String label, int quantity) {
-        if(!products.containsKey(label)) {
-            throw new IllegalArgumentException();
-        }
+        validateLabelExists(label);
         Product product = products.get(label);
         product.setQuantity(product.getQuantity() + quantity);
 
@@ -46,36 +45,65 @@ public class Instock implements ProductStock {
 
     @Override
     public Product findByLabel(String label) {
-        return null;
+        validateLabelExists(label);
+        return products.get(label);
     }
 
     @Override
     public Iterable<Product> findFirstByAlphabeticalOrder(int count) {
-        return null;
+        return fetchFirstCountComparedBy(count, Comparator.comparing(Product::getLabel));
     }
 
     @Override
     public Iterable<Product> findAllInRange(double lo, double hi) {
-        return null;
+        return this.products.values()
+                .stream()
+                .filter(p -> p.getPrice() > lo && p.getPrice() <= hi)
+                .sorted(Comparator.comparingDouble(Product::getPrice).reversed())
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Product> findAllByPrice(double price) {
-        return null;
+        return products.values()
+                .stream()
+                .filter(p -> p.getPrice() == price)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Product> findFirstMostExpensiveProducts(int count) {
-        return null;
+        return fetchFirstCountComparedBy(count, Comparator.comparingDouble(Product::getPrice).reversed());
     }
 
     @Override
     public Iterable<Product> findAllByQuantity(int quantity) {
-        return null;
+        return products.values()
+                .stream()
+                .filter(p -> p.getQuantity() == quantity)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterator<Product> iterator() {
-        return null;
+        return products.values().iterator();
+    }
+
+    private Iterable<Product> fetchFirstCountComparedBy(int count, Comparator<Product> comparator) {
+        if (count <= 0 || count > this.getCount()) {
+            return new ArrayList<>();
+        }
+
+        return products.values()
+                .stream()
+                .sorted(comparator)
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+
+    private void validateLabelExists(String label) {
+        if (!products.containsKey(label)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
