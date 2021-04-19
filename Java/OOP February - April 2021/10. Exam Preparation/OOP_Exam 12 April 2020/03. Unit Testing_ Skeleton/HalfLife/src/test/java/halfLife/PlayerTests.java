7fa64a65fd6691cb33713c1b7e2dce3761b75edd
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-04-19 20:01:05 +0300

    Add files via upload

--- Java/OOP February - April 2021/10. Exam Preparation/OOP_Exam 12 April 2020/03. Unit Testing_ Skeleton/HalfLife/src/test/java/halfLife/PlayerTests.java
+++ Java/OOP February - April 2021/10. Exam Preparation/OOP_Exam 12 April 2020/03. Unit Testing_ Skeleton/HalfLife/src/test/java/halfLife/PlayerTests.java
@@ -1,60 +1,49 @@
 package halfLife;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerTests {
-    //TODO: TEST ALL THE FUNCTIONALITY OF THE PROVIDED CLASS Player
+    private Player player;
 
-    @Test(expected = NullPointerException.class)
-    public void testConstructor_then_throwException1() {
-        Player player = new Player(null, 10);
-
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testConstructor_then_throwException2() {
-        Player player = new Player("Nikolay", -10);
-
-    }
-
-    @Test(expected = UnsupportedOperationException.class)
-    public void testGetList_throwException() {
-        Player player = new Player("Nikolay", 100);
+    @Before
+    public void setUp() {
+        player = new Player("Nikolay", 100);
         player.addGun(new Gun("A1", 10));
         player.addGun(new Gun("A2", 10));
         player.addGun(new Gun("pistol", 30));
+    }
 
+    @Test(expected = NullPointerException.class)
+    public void when_testConstructorWithNull_then_throwException1() {
+        player = new Player(null, 10);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void when_testConstructorWithNegative_then_throwException2() {
+        player = new Player("Nikolay", -10);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void when_GetListAndAdd_then_throwException() {
         player.getGuns().add(new Gun("pistol2", 50));
     }
 
     @Test
     public void when_getGun_then_returnCorrectGun() {
-        Player player = new Player("Nikolay", 100);
-        player.addGun(new Gun("A1", 10));
-        player.addGun(new Gun("A2", 10));
-        player.addGun(new Gun("pistol", 30));
-
         Gun expected = new Gun("pistol", 30);
         Gun actual = player.getGun("pistol");
-
         Assert.assertEquals(expected.getName(), actual.getName());
     }
 
     @Test
     public void when_getGun_then_returnNull() {
-        Player player = new Player("Nikolay", 100);
-        player.addGun(new Gun("A1", 10));
-        player.addGun(new Gun("A2", 10));
-        player.addGun(new Gun("pistol", 30));
-
-        Gun expected = null;
-        Gun actual = player.getGun("rifle");
-
-        Assert.assertNull(actual);
+        Gun gun = player.getGun("rifle");
+        Assert.assertNull(gun);
     }
 
 
@@ -71,7 +60,6 @@ public class PlayerTests {
         Player player = new Player("Ivan", 5);
         player.takeDamage(3);
         int actual = player.getHealth();
-
         Assert.assertEquals(2, actual);
     }
 
@@ -79,17 +67,15 @@ public class PlayerTests {
     @Test
     public void when_removeExistingGun_then_returnTrue() {
         Player player = new Player("Nikolay", 100);
-        Gun gun1 = new Gun("G1", 20);
-        player.addGun(gun1);
-
-        Assert.assertTrue(player.removeGun(gun1));
+        Gun gun = new Gun("G1", 20);
+        player.addGun(gun);
+        Assert.assertTrue(player.removeGun(gun));
     }
 
     @Test
     public void testGetUsername() {
         Player player = new Player("Nikolay", 100);
         String actual = player.getUsername();
-
         Assert.assertEquals("Nikolay", actual);
     }
 
@@ -97,13 +83,12 @@ public class PlayerTests {
     public void testGetHealth() {
         Player player = new Player("Nikolay", 100);
         int actual = player.getHealth();
-
-        Assert.assertEquals(100, actual);
+        int expected = 100;
+        Assert.assertEquals(expected, actual);
     }
 
     @Test(expected = NullPointerException.class)
     public void when_addGunNull_then_throwException() {
-        Player player = new Player("Nikolay", 100);
         player.addGun(null);
     }
 
