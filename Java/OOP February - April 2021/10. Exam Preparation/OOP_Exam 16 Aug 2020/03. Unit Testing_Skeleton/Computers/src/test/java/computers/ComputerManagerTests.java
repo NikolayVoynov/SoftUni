88183f1eab9f46ee9510a316bb06fea0bf6d1df5
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-04-21 22:02:18 +0300

    Add files via upload

--- Java/OOP February - April 2021/10. Exam Preparation/OOP_Exam 16 Aug 2020/03. Unit Testing_Skeleton/Computers/src/test/java/computers/ComputerManagerTests.java
+++ Java/OOP February - April 2021/10. Exam Preparation/OOP_Exam 16 Aug 2020/03. Unit Testing_Skeleton/Computers/src/test/java/computers/ComputerManagerTests.java
@@ -1,45 +1,44 @@
 package computers;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerManagerTests {
+    private ComputerManager computerManager;
+
+    @Before
+    public void setUp() {
+        this.computerManager = new ComputerManager();
+        this.computerManager.addComputer(new Computer("Dell", "A1", 1500.0));
+        this.computerManager.addComputer(new Computer("HP", "HP1", 1900.0));
+        this.computerManager.addComputer(new Computer("Asus", "ASS1", 2300.0));
+    }
 
     @Test
     public void when_testConstructor_then_returnCorrectArrayList() {
-        ComputerManager computerManager = new ComputerManager();
-        computerManager.addComputer(new Computer("Dell", "A1", 1500.0));
-        computerManager.addComputer(new Computer("HP", "HP1", 1900.0));
-        computerManager.addComputer(new Computer("Asus", "ASS1", 2300.0));
-
         List<Computer> expected = new ArrayList<>();
         expected.add(new Computer("Dell", "A1", 1500.0));
         expected.add(new Computer("HP", "HP1", 1900.0));
         expected.add(new Computer("Asus", "ASS1", 2300.0));
 
         int expectedSize = expected.size();
-        int actualSize = computerManager.getCount();
+        int actualSize = this.computerManager.getCount();
 
         Assert.assertEquals(expectedSize, actualSize);
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void when_getComputersAddComputer_throwException() {
-        ComputerManager computerManager = new ComputerManager();
-        computerManager.getComputers().add(new Computer("Asus", "ASS1", 2300.0));
+        this.computerManager.getComputers().add(new Computer("Asus", "ASS1", 2300.0));
     }
 
     @Test
     public void when_getComputersByManufacturer_then_returnCorrectList() {
-        ComputerManager computerManager = new ComputerManager();
-        computerManager.addComputer(new Computer("Dell", "A1", 1500.0));
-        computerManager.addComputer(new Computer("HP", "HP1", 1900.0));
-        computerManager.addComputer(new Computer("Asus", "ASS1", 2300.0));
-
-        List<Computer> expected = computerManager.getComputersByManufacturer("Asus");
+        List<Computer> expected = this.computerManager.getComputersByManufacturer("Asus");
         List<Computer> actual = new ArrayList<>();
         actual.add(new Computer("Asus", "ASS1", 2300.0));
 
@@ -48,12 +47,7 @@ public class ComputerManagerTests {
 
     @Test
     public void when_removeComputer_then_removeCorrectOne() {
-        ComputerManager computerManager = new ComputerManager();
-        computerManager.addComputer(new Computer("Dell", "A1", 1500.0));
-        computerManager.addComputer(new Computer("HP", "HP1", 1900.0));
-        computerManager.addComputer(new Computer("Asus", "ASS1", 2300.0));
-
-        Computer actualRemovedComputer = computerManager.removeComputer("Asus", "ASS1");
+        Computer actualRemovedComputer = this.computerManager.removeComputer("Asus", "ASS1");
         String expectedRemovedComputer = "ASS1";
         Assert.assertEquals(expectedRemovedComputer, actualRemovedComputer.getModel());
     }
