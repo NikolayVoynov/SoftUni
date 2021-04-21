90918ea4a51373e9f0528a014914815519a74a20
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-04-21 08:50:14 +0300

    Add files via upload

--- Java/OOP February - April 2021/10. Exam Preparation/OOP_Exam 20 Aug 2020/03. Unit Testing_Skeleton/Store/src/test/java/store/StoreTests.java
+++ Java/OOP February - April 2021/10. Exam Preparation/OOP_Exam 20 Aug 2020/03. Unit Testing_Skeleton/Store/src/test/java/store/StoreTests.java
@@ -1,74 +1,63 @@
 package store;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class StoreTests {
+    private Store store;
+
+    @Before
+    public void setUp() {
+        this.store = new Store();
+        this.store.addProduct(new Product("Tire", 2, 80));
+        this.store.addProduct(new Product("Oil", 2, 200));
+        this.store.addProduct(new Product("Mirror", 2, 280));
+    }
 
     @Test
     public void when_testConstructor_then_expectCorrectArrayList() {
-        Store store = new Store();
-        store.addProduct(new Product("Tire", 1, 80));
-        store.addProduct(new Product("Oil", 2, 200));
-        store.addProduct(new Product("Mirror", 2, 280));
-
-        List<Product> actual = store.getProducts();
-
+        List<Product> actual = this.store.getProducts();
         List<Product> expected = new ArrayList<>();
-        expected.add(new Product("Tire", 1, 80));
+        expected.add(new Product("Tire", 2, 80));
         expected.add(new Product("Oil", 2, 200));
         expected.add(new Product("Mirror", 2, 280));
 
-        Assert.assertEquals(expected.size(), store.getCount());
+        Assert.assertEquals(expected.size(), actual.size());
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void when_getProductsAndAdd_then_throwException() {
-        Store store = new Store();
-        store.getProducts().add(new Product("Seat", 2, 100));
+        this.store.getProducts().add(new Product("Seat", 2, 100));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void when_testAddProductEqualNull_then_throwException() {
-        Store store = new Store();
-        store.addProduct(null);
+        this.store.addProduct(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void when_testAddProductAndProductQuantityLessThanZero_then_throwException() {
-        Store store = new Store();
-        store.addProduct(new Product("Engine", -4, 3000));
+        this.store.addProduct(new Product("Engine", -4, 3000));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void when_testBuyProductReturnNull_then_throwException() {
-        Store store = new Store();
-        store.addProduct(new Product("Tire", 1, 80));
-        store.addProduct(new Product("Oil", 2, 200));
-
-        store.buyProduct("Engine", 1);
+        this.store.buyProduct("Engine", 1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void when_testBuyProductAndProductQuantityLess_then_throwException() {
-        Store store = new Store();
-        store.addProduct(new Product("Tire", 1, 80));
-        store.addProduct(new Product("Oil", 2, 200));
-
-        store.buyProduct("Oil", 10);
+        this.store.buyProduct("Oil", 10);
     }
 
     @Test
     public void when_testBuyProduct_then_returnFinalPrice() {
-        Store store = new Store();
-        store.addProduct(new Product("Tire", 4, 80.0));
-        store.addProduct(new Product("Oil", 2, 200.0));
-
         double expectedFinalPrice = 160.0;
-        double actualFinalPrice = store.buyProduct("Tire", 2);
+        double actualFinalPrice = this.store.buyProduct("Tire", 2);
 
         boolean equalPrice = false;
 
@@ -81,16 +70,12 @@ public class StoreTests {
 
     @Test
     public void when_getTheMostExpensiveProduct_then_returnCorrect() {
-        Store store = new Store();
-        store.addProduct(new Product("Tire", 1, 80));
-        store.addProduct(new Product("Oil", 2, 200));
-
-        Product expected = new Product("Oil", 2, 200);
-        Product actual = store.getTheMostExpensiveProduct();
+        Product expected = new Product("Mirror", 2, 280);
+        Product actual = this.store.getTheMostExpensiveProduct();
 
         String expectedName = expected.getName();
         String actualName = actual.getName();
-        Assert.assertEquals(expectedName,actualName);
+        Assert.assertEquals(expectedName, actualName);
     }
 
 
