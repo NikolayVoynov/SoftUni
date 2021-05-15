2efe84f00a3c486aab1df00fd91417be1f8e6dd8
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-05-15 15:14:33 +0300

    Add files via upload

---
+++ Java/Fundamentals September - December 2020/11 More Exercise/02. Data Types and Variables/ME03FloatingEquality.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ME03FloatingEquality {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double n1 = Double.parseDouble(scanner.nextLine());
+        double n2 = Double.parseDouble(scanner.nextLine());
+        double eps = 0.000001;
+
+        if (Math.abs(n1 -n2) < eps) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
