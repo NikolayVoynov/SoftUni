77cca35f1e343a111f4ae854b864f11821f7b78c
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-06-05 19:50:59 +0300

    Add files via upload

---
+++ Java/Data Base May - August 2021/MySQL/05. Table Relations/Problem 04 - Self-Referencing.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `teachers` (
+`teacher_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(30),
+`manager_id` INT,
+CONSTRAINT `fk_manager_teacher`
+FOREIGN KEY (`manager_id`)
+REFERENCES `teachers`(`teacher_id`)
+);
+
+INSERT INTO `teachers`
+VALUES
+(101,'John',NULL),	
+(102,'Maya',106),
+(103,'Silvia',106),
+(104,'Ted',105),
+(105,'Mark',101),
+(106,'Greta',101);
+
+
