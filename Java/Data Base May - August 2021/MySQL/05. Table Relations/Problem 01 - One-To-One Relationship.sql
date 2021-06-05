908b8c636d6a4840db191b016634f2602c52aaa1
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-06-05 19:02:03 +0300

    Add files via upload

---
+++ Java/Data Base May - August 2021/MySQL/05. Table Relations/Problem 01 - One-To-One Relationship.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `passports`(
+`passport_id` INT PRIMARY KEY AUTO_INCREMENT,
+`passport_number` VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE `people`(
+`person_id` INT PRIMARY KEY AUTO_INCREMENT,
+`first_name` VARCHAR(50),
+`salary` DOUBLE,
+`passport_id` INT,
+CONSTRAINT `fk_people_passports`
+FOREIGN KEY (`passport_id`)
+REFERENCES `passports`(`passport_id`)
+);
+
+INSERT INTO `passports`
+VALUES
+(101, 'N34FG21B'),
+(102, 'K65LO4R7'),
+(103, 'ZE657QP2');
+
+INSERT INTO `people`
+VALUES
+(1,'Roberto',43300.00,102),
+(2,'Tom',56100.00,103),
+(3,	'Yana',60200.00,101);
