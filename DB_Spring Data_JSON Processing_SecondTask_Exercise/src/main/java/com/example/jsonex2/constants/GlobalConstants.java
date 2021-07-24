827ce95fab2eb97983fbcd123cb0df0921f92841
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-07-24 14:27:29 +0300

    Delete DB_Spring Data_JSON Processing_SecondTask_Exercise directory

--- DB_Spring Data_JSON Processing_SecondTask_Exercise/src/main/java/com/example/jsonex2/constants/GlobalConstants.java
+++
@@ -1,6 +0,0 @@
-package com.example.jsonex2.constants;
-
-public class GlobalConstants {
-
-    public static final String RESOURCES_FILE_PATH = "src/main/resources/files/";
-}
--- DB_Spring Data_JSON Processing_SecondTask_Exercise/src/main/java/com/example/jsonex2/util/ValidationUtil.java
+++
@@ -1,6 +0,0 @@
-package com.example.jsonex2.util;
-
-public interface ValidationUtil {
-
-    <E> boolean isValid(E entity);
-}
