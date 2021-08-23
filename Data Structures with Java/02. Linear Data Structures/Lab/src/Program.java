f7a1142016a34ada48de23e677e9b05c986c67e7
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-08-23 23:07:04 +0300

    Add files via upload

--- Data Structures with Java/02. Linear Data Structures/Lab/src/Program.java
+++ Data Structures with Java/02. Linear Data Structures/Lab/src/Program.java
@@ -4,24 +4,55 @@ import java.util.Arrays;
 public class Program {
 
     class DynamicArray<E> {
-        boolean add(E element) {
+        private Object[] elements = new Object[1];
+        private int size = 0;
 
+        boolean add(E element) {
+            elements[size] = element;
+            size++;
+
+            if (size >= elements.length) {
+                Object[] newElements = new Object[size * 2];
+
+                for (int i = 0; i < elements.length; i++) {
+                    newElements[i] = elements[i];
+                }
+
+                elements = newElements;
+            }
+
+            return true;
         }
 
         E get(int index) {
+            checkIndex(index);
+            return (E) elements[index];
 
+        }
+
+        private void checkIndex(int index) {
+            if (index < 0 || index >= size) {
+                throw new IllegalArgumentException();
+            }
         }
 
         E set(int index, E element) {
-
+            elements[index] = element;
+            return element;
         }
 
         E remove(int index) {
+            E removedElement = (E) elements[index];
+            for (int i = index; i < size - 1; i++) {
+                elements[i] = elements[i + 1];
+            }
 
+            size--;
+            return removedElement;
         }
 
         int size() {
-
+            return this.size;
         }
     }
 
