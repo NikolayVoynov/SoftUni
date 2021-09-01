e1a5ec71651fd0e1980969601ace123e7930528d
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-09-01 13:11:54 +0300

    Add files via upload

--- Data Structures with Java/03. Trees Representation and Traversal (BFS, DFS)/Lab/src/DirectoryTree.java
+++ Data Structures with Java/03. Trees Representation and Traversal (BFS, DFS)/Lab/src/DirectoryTree.java
@@ -1,10 +1,41 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class DirectoryTree {
 
     static class Directory {
-        
+        String name;
+        List<Directory> subdirectories;
+        Directory parent;
+
+        public Directory(String name, List<Directory> subdirectories) {
+            this.name = name;
+            this.subdirectories = subdirectories;
+            if (subdirectories != null) {
+                for (Directory subdirectory : subdirectories) {
+                    subdirectory.parent = this;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
+
+        Directory root = new Directory("C", Arrays.asList(
+                new Directory("AMD", null),
+                new Directory("BIOS", null),
+                new Directory("drivers", null),
+                new Directory("Intel", null),
+                new Directory("PerfLogs", null),
+                new Directory("Program Files", Arrays.asList(
+                        new Directory("7-Zip", null),
+                        new Directory("AWAST Software", null),
+                        new Directory("CDBurnerXP", null)
+                )),
+                new Directory("Program Files (x86)", null)
+        ));
+
+        System.out.println();
 
     }
 }
