ba850ed2cf65e7552906ab237028ff194eb48b36
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-09-06 22:28:51 +0300

    Add files via upload

--- Data Structures with Java/04. Heaps, BST/Lab/src/HeapProg.java
+++ Data Structures with Java/04. Heaps, BST/Lab/src/HeapProg.java
@@ -28,15 +28,77 @@ public class HeapProg {
         }
 
         public boolean isEmpty() {
-            return getSize() > 0;
+            return getSize() == 0;
         }
 
         public void push(K key, V value) {
+            Element<K, V> element = new Element<>(key, value);
+            elements.add(element);
+
+            swim(elements.size() - 1);
+        }
+
+        private boolean isValidIndex(int index) {
+            return index >= 0 && index < elements.size();
+        }
+
+        private void swim(int index) {
+            if (!isValidIndex(index)) {
+                return;
+            }
+
+            if (index == 0) {
+                return;
+            }
+
+            int parentIndex = (index - 1) / 2;
+
+            if (isLessThan(parentIndex, index)) {
+                swap(index, parentIndex);
+                swim(parentIndex);
+            }
 
         }
 
+        private void swap(int index, int parentIndex) {
+            Element<K, V> oldValue = elements.get(index);
+            elements.set(index, elements.get(parentIndex));
+            elements.set(parentIndex, oldValue);
+        }
+
+        private boolean isLessThan(int index, int parentIndex) {
+            return elements.get(index).getKey().compareTo(elements.get(parentIndex).getKey()) < 0;
+        }
+
         public Element<K, V> popMax() {
-            return null;
+            Element<K, V> max = elements.get(0);
+            elements.set(0, elements.get(elements.size() - 1));
+            elements.remove(elements.size() - 1);
+
+            sink(0);
+            return max;
+        }
+
+        private void sink(int index) {
+            int indexFirstChild = index * 2 + 1;
+            int indexSecondChild = index * 2 + 2;
+
+            if (!isValidIndex(indexFirstChild)) {
+                return;
+            }
+
+            int maxIndex;
+            if (isValidIndex(indexSecondChild) && isLessThan(indexFirstChild,indexSecondChild)) {
+                maxIndex = indexSecondChild;
+            } else {
+                maxIndex = indexFirstChild;
+            }
+
+            if (isLessThan(index, maxIndex)) {
+                swap(index, maxIndex);
+                sink(maxIndex);
+            }
+
         }
 
     }
