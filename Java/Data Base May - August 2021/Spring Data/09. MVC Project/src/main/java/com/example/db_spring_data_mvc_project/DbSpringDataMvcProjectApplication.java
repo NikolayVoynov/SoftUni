0c1502e6cda95b2561f26727bbec6b23a517a70b
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-09-09 09:00:48 +0300

    Add files via upload

---
+++ Java/Data Base May - August 2021/Spring Data/09. MVC Project/src/main/java/com/example/db_spring_data_mvc_project/DbSpringDataMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.db_spring_data_mvc_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSpringDataMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbSpringDataMvcProjectApplication.class, args);
+	}
+
+}
---
+++ Java/Data Base May - August 2021/Spring Data/09. MVC Project/src/test/java/com/example/db_spring_data_mvc_project/DbSpringDataMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.db_spring_data_mvc_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSpringDataMvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
