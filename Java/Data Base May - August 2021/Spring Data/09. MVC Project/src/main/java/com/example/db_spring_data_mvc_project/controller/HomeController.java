a054b1fdb065022a59bafdb894b9db6877108a65
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-09-13 23:01:22 +0300

    Add files via upload

--- Java/Data Base May - August 2021/Spring Data/09. MVC Project/src/main/java/com/example/db_spring_data_mvc_project/controller/HomeController.java
+++ Java/Data Base May - August 2021/Spring Data/09. MVC Project/src/main/java/com/example/db_spring_data_mvc_project/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.db_spring_data_mvc_project.controller;
 
+import com.example.db_spring_data_mvc_project.service.CompanyService;
+import com.example.db_spring_data_mvc_project.service.EmployeeService;
+import com.example.db_spring_data_mvc_project.service.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController extends BaseController {
+
+    private final EmployeeService employeeService;
+    private final CompanyService companyService;
+    private final ProjectService projectService;
+
+    public HomeController(EmployeeService employeeService, CompanyService companyService,
+                          ProjectService projectService) {
+        this.employeeService = employeeService;
+        this.companyService = companyService;
+        this.projectService = projectService;
+    }
+
 
     @GetMapping("/")
     public String index(HttpServletRequest request) {
