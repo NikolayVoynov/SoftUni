c8f2a5a3f002ee14fefbd2b5b1b25dad149a9c4d
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-09-25 00:13:15 +0300

    Add files via upload

--- Java/Data Base May - August 2021/Spring Data/09. MVC Project/src/main/java/com/example/db_spring_data_mvc_project/controller/UserController.java
+++ Java/Data Base May - August 2021/Spring Data/09. MVC Project/src/main/java/com/example/db_spring_data_mvc_project/controller/UserController.java
@@ -3,15 +3,19 @@ package com.example.db_spring_data_mvc_project.controller;
 import com.example.db_spring_data_mvc_project.dto.UserLoginDto;
 import com.example.db_spring_data_mvc_project.dto.UserRegisterDto;
 import com.example.db_spring_data_mvc_project.service.UserService;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 @Controller
-public class UserController {
+public class UserController extends BaseController {
 
     private final UserService userService;
 
@@ -35,12 +39,18 @@ public class UserController {
     }
 
     @GetMapping("/users/login")
-    public String login() {
+    public String login(UserLoginDto user, Model model) {
+        model.addAttribute("userLoginForm", user);
         return "user/login";
     }
 
     @PostMapping("/users/login")
-    public String login(UserLoginDto user, Model model, HttpServletRequest request) {
+    public String login(@Valid UserLoginDto user, Model model, HttpServletRequest request, BindingResult result) {
+        if (result.hasErrors()) {
+            model.addAttribute("errors", result.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList()));
+            return this.login(user, model);
+        }
+
         var userId = this.userService.validateUserLoginDetails(user);
 
         if (userId == null) {
@@ -49,6 +59,18 @@ public class UserController {
         }
 
         request.getSession().setAttribute("userId", userId);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        if (this.isLogged(request)) {
+            request.getSession().setAttribute(
+                    "userId",
+                    null
+            );
+        }
 
         return "redirect:/";
     }
