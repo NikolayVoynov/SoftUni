df3ecd6943a08dd1c3891756c67583826b98b1e9
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-09-27 09:33:54 +0300

    Add files via upload

---
+++ Java/Java Web September - December 2021/Spring Fundamentals/mobilelele/src/main/java/bg/softuni/mobilelele/model/entity/BrandEntity.java
@@ -0,0 +1,31 @@
+package bg.softuni.mobilelele.model.entity;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.List;
+
+@Entity
+@Table(name = "brands")
+public class BrandEntity extends BaseEntity {
+
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<ModelEntity> models;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ModelEntity> getModels() {
+        return models;
+    }
+
+    public void setModels(List<ModelEntity> models) {
+        this.models = models;
+    }
+}
---
+++ Java/Java Web September - December 2021/Spring Fundamentals/mobilelele/src/main/java/bg/softuni/mobilelele/model/entity/CategoryEnum.java
@@ -0,0 +1,7 @@
+package bg.softuni.mobilelele.model.entity;
+
+public enum CategoryEnum {
+
+    CAR, BUS, TRUCK, MOTORCYCLE
+
+}
