7480968de8dccaa0caef1de12a03445e7bb2f598
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-10-01 00:01:18 +0300

    Add files via upload

---
+++ Java/Java Web September - December 2021/Spring Fundamentals/Workshop 01/src/main/java/com/example/pathfinder/model/entity/Role.java
@@ -0,0 +1,13 @@
+package com.example.pathfinder.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity {
+
+
+
+
+}
