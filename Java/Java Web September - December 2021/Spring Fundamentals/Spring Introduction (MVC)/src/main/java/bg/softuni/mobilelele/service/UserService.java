a3e2cd5478d58e85afa833f0f9b42d3e10d09aad
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-10-03 13:32:45 +0300

    Delete Java/Java Web September - December 2021/Spring Fundamentals/Spring Introduction (MVC) directory

--- Java/Java Web September - December 2021/Spring Fundamentals/Spring Introduction (MVC)/src/main/java/bg/softuni/mobilelele/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package bg.softuni.mobilelele.service;
-
-import bg.softuni.mobilelele.model.service.UserLoginServiceModel;
-
-public interface UserService {
-    boolean login(UserLoginServiceModel loginServiceModel);
-}
