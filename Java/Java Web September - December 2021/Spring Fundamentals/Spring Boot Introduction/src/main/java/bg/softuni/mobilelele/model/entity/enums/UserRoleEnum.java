54d86cdd87e3865c3a2474da8fadd94bba18ca63
NikolayVoynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-10-03 13:32:23 +0300

    Delete Java/Java Web September - December 2021/Spring Fundamentals/Spring Boot Introduction directory

--- Java/Java Web September - December 2021/Spring Fundamentals/Spring Boot Introduction/src/main/java/bg/softuni/mobilelele/model/entity/enums/UserRoleEnum.java
+++
@@ -1,5 +0,0 @@
-package bg.softuni.mobilelele.model.entity.enums;
-
-public enum UserRoleEnum {
-    USER, ADMIN;
-}
--- Java/Java Web September - December 2021/Spring Fundamentals/Spring Boot Introduction/src/main/java/bg/softuni/mobilelele/service/ModelService.java
+++
@@ -1,7 +0,0 @@
-package bg.softuni.mobilelele.service;
-
-public interface ModelService {
-
-    void initializeModels();
-
-}
