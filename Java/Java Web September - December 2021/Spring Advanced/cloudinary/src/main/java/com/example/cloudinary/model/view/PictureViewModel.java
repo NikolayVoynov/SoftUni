3365135788e37701b6dc1bed6189c9b1f44ab87d
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2021-11-09 09:30:08 +0200

    Add files via upload

---
+++ Java/Java Web September - December 2021/Spring Advanced/cloudinary/src/main/java/com/example/cloudinary/model/view/PictureViewModel.java
@@ -0,0 +1,35 @@
+package com.example.cloudinary.model.view;
+
+public class PictureViewModel {
+
+    private String title;
+    private String url;
+    private String publicId;
+
+    public PictureViewModel() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(String publicId) {
+        this.publicId = publicId;
+    }
+}
