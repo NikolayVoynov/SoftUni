442c93c3087e66facdaa57d8736e4037b7589a77
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2022-01-16 08:27:30 +0200

    Delete L02_StringLength.js

--- JavaScript/JS Advanced Jan-Feb 2022/01 Syntax, Functions and Statements/L02_StringLength.js
+++
@@ -1,9 +0,0 @@
-function sumAverageLength(str1,str2,str3) {
-    let sumLength = str1.length + str2.length + str3.length;
-    let averageLength = Math.floor(sumLength / 3);
-
-    console.log(sumLength);
-    console.log(averageLength);
-}
-
-sumAverageLength('chocolate', 'ice cream', 'cake')
