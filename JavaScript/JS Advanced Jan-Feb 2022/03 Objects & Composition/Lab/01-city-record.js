687cbe8585327d5db06861d600e43956dfd8732d
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2022-01-19 21:26:58 +0200

    Add files via upload

---
+++ JavaScript/JS Advanced Jan-Feb 2022/03 Objects & Composition/Lab/01-city-record.js
@@ -0,0 +1,9 @@
+function cityRecord(name, population, treasury) {
+    const cityInfo = {}
+
+    cityInfo.name = name;
+    cityInfo.population = population;
+    cityInfo.treasury = treasury;
+
+    return cityInfo;
+}
