05d6647bab79be37e527082012df87150c1e61f5
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2022-02-07 07:29:59 +0200

    Add files via upload

---
+++ JavaScript/JS Advanced Jan-Feb 2022/08 Classes/Lab/02-get-persons.js
@@ -0,0 +1,21 @@
+function arrayOfPersons() {
+
+    class Person {
+
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+    }
+
+    let person1 = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    let person2 = new Person('SoftUni');
+    let person3 = new Person('Stephan', 'Johnson', 25);
+    let person4 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    console.log([person1, person2, person3, person4]);
+}
+
