eef4bcaa3b22ea26b76a5bca1265daf8c87cef59
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2022-02-15 19:23:54 +0200

    Add files via upload

--- JavaScript/JS Advanced Jan-Feb 2022/10 Exams/13.02-final-exam-27-jun-2021/restaurant.js
+++ JavaScript/JS Advanced Jan-Feb 2022/10 Exams/13.02-final-exam-27-jun-2021/restaurant.js
@@ -95,7 +95,7 @@ class Restaurant {
                 for (const productData of productsForMeal) {
                     let array = productData.split(' ');
                     let product = array[0];
-                    let quantity = array[1];
+                    let quantity = Number(array[1]);
 
                     this.stockProducts[product] -= quantity;
                 }
@@ -104,29 +104,7 @@ class Restaurant {
             } else {
                 return `For the time being, we cannot complete your order (${meal}), we are very sorry...`;
             }
-
         }
-
     };
 }
-
-
-// let kitchen = new Restaurant(1000);
-// console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 50 30', 'Yogurt 10 10', 'Yogurt 500 1500', 'Honey 5 50']));
-
-// let kitchen = new Restaurant(1000);
-// console.log(kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99));
-// console.log(kitchen.addToMenu('Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Salt 0.1', 'Sugar 0.1', 'Tomato sauce 0.5', 'Pepperoni 1', 'Cheese 1.5'], 15.55));
-
-// let kitchen = new Restaurant(1000);
-// console.log(kitchen.showTheMenu());
-
-
-let kitchen = new Restaurant(1000);
-kitchen.loadProducts(['Yogurt 30 3', 'Honey 50 4', 'Strawberries 20 10', 'Banana 5 1']);
-kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99);
-console.log(kitchen.makeTheOrder('frozenYogurt'));
-
-
-// || this.stockProducts.product < quantity
 
