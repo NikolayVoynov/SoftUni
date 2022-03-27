b2687b604a8ea2dd7bbdaeb3e458c9342d13c3e1
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2022-03-27 11:53:40 +0300

    Add files via upload

---
+++ JavaScript/JS Applications/exam_04_dec_2021_prep/src/utils/validators.js
@@ -0,0 +1,8 @@
+
+
+
+export const albumIsInvalid = (albumData) => {
+    const requiredField = ['name', 'imgUrl', 'price', 'releaseDate', 'artist', 'genre', 'description'];
+
+    return requiredField.some(x => !albumData[x]);
+}
