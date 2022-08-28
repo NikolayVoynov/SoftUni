8d3f1bd5ded26cbcb85667cb381b840c22a6e76f
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2022-08-28 20:18:35 +0300

    Add files via upload

---
+++ Java/Additional Exercise 2/Basic 1/E46CurrentSystemTime.java
@@ -0,0 +1,8 @@
+package Basic1;
+
+public class E46CurrentSystemTime {
+
+    public static void main(String[] args) {
+        System.out.printf("Current system time is: %tc%n", System.currentTimeMillis());
+    }
+}
