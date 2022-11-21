d595a97e7787cdc156deea6be9327960b47aab43
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2022-11-21 23:03:37 +0200

    Add files via upload

---
+++ Data Structures with Java/03. Trees Representation and Traversal (BFS, DFS)/Exercise 2022/src/main/java/implementations/TheMatrix.java
@@ -0,0 +1,96 @@
+package implementations;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class TheMatrix {
+    private char[][] matrix;
+    private char fillChar;
+    private char startChar;
+    private int startRow;
+    private int startCol;
+
+    public TheMatrix(char[][] matrix, char fillChar, int startRow, int startCol) {
+        this.matrix = matrix;
+        this.fillChar = fillChar;
+        this.startRow = startRow;
+        this.startCol = startCol;
+        this.startChar = this.matrix[this.startRow][this.startCol];
+    }
+
+    public void solve() {
+
+        //       --- Solution with recursion ---
+
+//        fillMatrix(this.startRow, this.startCol);
+
+        //        --- Solution with Deque ---
+
+        Deque<int[]> coordinates = new ArrayDeque<>();
+
+        coordinates.offer(new int[]{startRow, startCol});
+
+        while (!coordinates.isEmpty()) {
+            int[] position = coordinates.poll();
+
+            int row = position[0];
+            int col = position[1];
+
+            this.matrix[row][col] = this.fillChar;
+
+            if (!isOutOfBounds(row - 1, col) && this.matrix[row - 1][col] == this.startChar) {
+                coordinates.offer(new int[]{row - 1, col});
+            }
+
+            if (!isOutOfBounds(row, col + 1) && this.matrix[row][col + 1] == this.startChar) {
+                coordinates.offer(new int[]{row, col + 1});
+            }
+
+            if (!isOutOfBounds(row + 1, col) && this.matrix[row + 1][col] == this.startChar) {
+                coordinates.offer(new int[]{row + 1, col});
+            }
+
+            if (!isOutOfBounds(row, col - 1) && this.matrix[row][col - 1] == this.startChar) {
+                coordinates.offer(new int[]{row, col - 1});
+            }
+        }
+
+    }
+
+    //        --- Solution with recursion ---
+
+//    private void fillMatrix(int row, int col) {
+//
+//        if (isOutOfBounds(row, col) || (this.matrix[row][col] != this.startChar)) {
+//            return;
+//        }
+//
+//        this.matrix[row][col] = this.fillChar;
+//
+//        System.out.println(this.toOutputString());
+//
+//        this.fillMatrix(row + 1, col);
+//        this.fillMatrix(row, col + 1);
+//        this.fillMatrix(row - 1, col);
+//        this.fillMatrix(row, col - 1);
+//
+//    }
+    //    ----------------------------------
+
+    private boolean isOutOfBounds(int row, int col) {
+        return row < 0 || row >= this.matrix.length || col < 0 || col >= this.matrix[row].length;
+    }
+
+    public String toOutputString() {
+        StringBuilder builder = new StringBuilder();
+
+        for (int r = 0; r < this.matrix.length; r++) {
+            for (int c = 0; c < this.matrix[r].length; c++) {
+                builder.append(this.matrix[r][c]);
+            }
+            builder.append(System.lineSeparator());
+        }
+
+        return builder.toString().trim();
+    }
+}
