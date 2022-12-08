4b126d6b5ffbad0523e0793106df96c8dcee821c
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2022-12-08 19:32:54 +0200

    Add files via upload

--- Data Structures with Java/07 Exam Preparation/src/main/java/core/Data.java
+++ Data Structures with Java/07 Exam Preparation/src/main/java/core/Data.java
@@ -3,33 +3,52 @@ package core;
 import interfaces.Entity;
 import interfaces.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Data implements Repository {
+    private List<Entity> dataList;
+
+    public Data() {
+        this.dataList = new ArrayList<>();
+    }
 
     @Override
     public void add(Entity entity) {
-
+        this.dataList.add(entity);
     }
 
     @Override
     public Entity getById(int id) {
-        return null;
+        Entity result = null;
+
+        for (Entity entity : dataList) {
+            if (entity.getId() == id) {
+                result = entity;
+            }
+        }
+
+        return result;
     }
 
     @Override
     public List<Entity> getByParentId(int id) {
-        return null;
+        return this.dataList
+                .stream()
+                .filter(entity -> entity.getParentId() == id)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Entity> getAll() {
-        return null;
+        return this.dataList;
     }
 
     @Override
     public Repository copy() {
-        return null;
+
+        return (Repository) List.copyOf(this.dataList);
     }
 
     @Override
@@ -39,16 +58,43 @@ public class Data implements Repository {
 
     @Override
     public Entity peekMostRecent() {
-        return null;
+        if (this.dataList.isEmpty()) {
+            throw new IllegalStateException("Operation on empty Data");
+        }
+
+        Entity entityGreatestId = null;
+
+        for (Entity entity : dataList) {
+            int id = Integer.MIN_VALUE;
+            if (entity.getId() > id) {
+                entityGreatestId = entity;
+            }
+        }
+
+        return entityGreatestId;
     }
 
     @Override
     public Entity pollMostRecent() {
-        return null;
+        if (this.dataList.isEmpty()) {
+            throw new IllegalStateException("Operation on empty Data");
+        }
+
+        Entity entityGreatestId = null;
+
+        for (Entity entity : dataList) {
+            int id = Integer.MIN_VALUE;
+            if (entity.getId() > id) {
+                entityGreatestId = entity;
+            }
+        }
+
+        this.dataList.remove(entityGreatestId);
+        return entityGreatestId;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.dataList.size();
     }
 }
--- Data Structures with Java/07 Exam Preparation/src/main/java/core/Loader.java
+++ Data Structures with Java/07 Exam Preparation/src/main/java/core/Loader.java
@@ -4,78 +4,170 @@ import interfaces.Buffer;
 import interfaces.Entity;
 import model.BaseEntity;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Loader implements Buffer {
+    private LinkedList<Entity> listEntities;
+//    private List<Entity> listEntities;
+
+    public Loader() {
+        this.listEntities = new LinkedList<>();
+//        this.listEntities = new ArrayList<>();
+    }
 
     @Override
     public void add(Entity entity) {
-
+        this.listEntities.add(entity);
     }
 
     @Override
     public Entity extract(int id) {
-        return null;
+        if (this.listEntities.isEmpty()) {
+            return null;
+        }
+
+        Entity removedEntity = null;
+
+        for (Entity entity : listEntities) {
+            if (entity.getId() == id) {
+                removedEntity = entity;
+                this.listEntities.remove(entity);
+                break;
+            }
+        }
+
+        if (removedEntity == null) {
+            return null;
+        }
+
+        return removedEntity;
     }
 
     @Override
     public Entity find(Entity entity) {
-        return null;
+        if (this.listEntities.isEmpty()) {
+            return null;
+        }
+
+        Entity foundEntity = null;
+
+        for (Entity listEntity : listEntities) {
+            if (listEntity == entity) {
+                foundEntity = listEntity;
+                break;
+            }
+        }
+
+        if (foundEntity == null) {
+            return null;
+        }
+
+        return foundEntity;
     }
 
     @Override
     public boolean contains(Entity entity) {
-        return false;
+        return this.find(entity) != null;
     }
 
     @Override
     public int entitiesCount() {
-        return 0;
+        return this.listEntities.size();
     }
 
     @Override
     public void replace(Entity oldEntity, Entity newEntity) {
+        Entity lookingFor = this.find(oldEntity);
 
+        if (lookingFor == null) {
+            throw new IllegalStateException("Entity not found");
+        }
+
+        // Дали е на същия индекс?
+
+        this.listEntities.remove(lookingFor);
+        this.listEntities.add(newEntity);
     }
 
     @Override
     public void swap(Entity first, Entity second) {
+        Entity findFirst = this.find(first);
+        Entity findSecond = this.find(second);
 
+        if (findFirst == null || findSecond == null) {
+            throw new IllegalStateException("Entities not found");
+        }
+
+        Entity temp = findFirst;
+        findFirst = findSecond;
+        findSecond = temp;
     }
 
     @Override
     public void clear() {
-
+        this.listEntities.clear();
     }
 
     @Override
     public Entity[] toArray() {
-        return new Entity[0];
+        return listEntities.toArray(new Entity[0]);
     }
 
     @Override
     public List<Entity> retainAllFromTo(BaseEntity.Status lowerBound, BaseEntity.Status upperBound) {
-        return null;
+        return this.listEntities
+                .stream()
+                .filter(e -> e.getStatus().ordinal() >= lowerBound.ordinal() && e.getStatus().ordinal() <= upperBound.ordinal())
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Entity> getAll() {
-        return null;
+        return this.listEntities;
     }
 
     @Override
     public void updateAll(BaseEntity.Status oldStatus, BaseEntity.Status newStatus) {
-
+        this.listEntities
+                .forEach(e -> {
+                    if (e.getStatus().equals(oldStatus)) {
+                        e.setStatus(newStatus);
+                    }
+                });
     }
 
     @Override
     public void removeSold() {
-
+        this.listEntities
+                .forEach(e -> {
+                    if (e.getStatus().equals(BaseEntity.Status.SOLD)) {
+                        listEntities.remove(e);
+                    }
+                });
     }
 
     @Override
     public Iterator<Entity> iterator() {
-        return null;
+        return new Iterator<Entity>() {
+            private int i = 0;
+            private LinkedList<Entity> list = listEntities;
+//            private List<Entity> list = listEntities;
+
+            @Override
+            public boolean hasNext() {
+                return i < list.size();
+            }
+
+            @Override
+            public Entity next() {
+                Entity currentEntity = list.get(i);
+                i++;
+                return currentEntity;
+            }
+        };
     }
 }
