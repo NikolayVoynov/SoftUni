6ac7806a72fa24c70c41628ae3fd46ccffafe4f8
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-01-22 22:06:31 +0200

    Add files via upload

--- Data Structures with Java/15. Exam Preparation/src/main/java/core/MoovItImpl.java
+++ Data Structures with Java/15. Exam Preparation/src/main/java/core/MoovItImpl.java
@@ -2,35 +2,58 @@ package core;
 
 import models.Route;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MoovItImpl implements MoovIt {
+    private Map<String, Route> routesById;
+
+    public MoovItImpl() {
+        this.routesById = new HashMap<>();
+    }
+
     @Override
     public void addRoute(Route route) {
-
+        if (contains(route)) {
+            throw new IllegalArgumentException();
+        }
+        this.routesById.put(route.getId(), route);
     }
 
     @Override
     public void removeRoute(String routeId) {
-
+        if (!this.routesById.containsKey(routeId)) {
+            throw new IllegalArgumentException();
+        }
+        this.routesById.remove(routeId);
     }
 
     @Override
     public boolean contains(Route route) {
-        return false;
+        return this.routesById.containsKey(route.getId());
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.routesById.size();
     }
 
     @Override
     public Route getRoute(String routeId) {
-        return null;
+        if (!this.routesById.containsKey(routeId)) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.routesById.get(routeId);
     }
 
     @Override
     public void chooseRoute(String routeId) {
+        if (!this.routesById.containsKey(routeId)) {
+            throw new IllegalArgumentException();
+        }
 
+        this.routesById.get(routeId).setPopularity(this.routesById.get(routeId).getPopularity() + 1);
     }
 
     @Override
