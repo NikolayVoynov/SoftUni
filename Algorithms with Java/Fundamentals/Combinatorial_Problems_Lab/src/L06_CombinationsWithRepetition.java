e74075d2670e38a764877f2e65dbd786e327ad5f
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-05-28 15:52:39 +0300

    Add files via upload

---
+++ Algorithms with Java/Fundamentals/Combinatorial_Problems_Lab/src/L06_CombinationsWithRepetition.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class L06_CombinationsWithRepetition {
+    public static String[] elements;
+    public static String[] kSlots;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        elements = scanner.nextLine().split("\\s+");
+        int k = Integer.parseInt(scanner.nextLine());
+
+        kSlots = new String[k];
+
+        createCombination(0, 0);
+    }
+
+    private static void createCombination(int index, int start) {
+        if (index >= kSlots.length) {
+            print();
+            return;
+        }
+
+        for (int i = start; i < elements.length; i++) {
+            kSlots[index] = elements[i];
+            createCombination(index + 1, i );
+
+        }
+
+    }
+
+    private static void print() {
+        System.out.println(String.join(" ", kSlots));
+    }
+}
