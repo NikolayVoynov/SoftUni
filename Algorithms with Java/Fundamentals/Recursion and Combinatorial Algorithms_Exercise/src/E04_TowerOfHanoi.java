71bc0c67ed90cb09a273d00ef7eb3ac83e51cc72
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-05-31 09:31:42 +0300

    Add files via upload

--- Algorithms with Java/Fundamentals/Recursion and Combinatorial Algorithms_Exercise/src/E04_TowerOfHanoi.java
+++ Algorithms with Java/Fundamentals/Recursion and Combinatorial Algorithms_Exercise/src/E04_TowerOfHanoi.java
@@ -7,11 +7,13 @@ import java.util.Deque;
 import java.util.stream.Collectors;
 
 public class E04_TowerOfHanoi {
+    public static StringBuilder builder = new StringBuilder();
+
     public static Deque<Integer> source = new ArrayDeque<>();
     public static Deque<Integer> spare = new ArrayDeque<>();
     public static Deque<Integer> destination = new ArrayDeque<>();
 
-    public static int steps = 0;
+    public static int steps = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -22,24 +24,31 @@ public class E04_TowerOfHanoi {
             source.push(i);
         }
 
-        solve(disk, source, spare, destination);
         printRods();
+        solve(disk, source, destination, spare);
+
+        System.out.println(builder.toString());
     }
 
-    public static void solve(int disk, Deque<Integer> source, Deque<Integer> spare, Deque<Integer> destination) {
+    public static void solve(int disk, Deque<Integer> source, Deque<Integer> destination, Deque<Integer> spare) {
         if (disk == 1) {
             destination.push(source.pop());
-            System.out.println("Step #" + (steps++) + ": Moved disk");
+            builder.append("Step #")
+                    .append(steps++)
+                    .append(": Moved disk")
+                    .append(System.lineSeparator());
             printRods();
         } else {
             solve(disk - 1, source, spare, destination);
+            solve(1, source, destination, spare);
+            solve(disk - 1, spare, destination, source);
         }
     }
 
     public static void printRods() {
-        System.out.println(String.format("Source: %s%n" +
-                "Destination: %s%n" +
-                "Spare: %s%n", formatRod(source), formatRod(spare), formatRod(destination)));
+        builder.append(String.format("Source: %s%nDestination: %s%nSpare: %s%n",
+                formatRod(source), formatRod(destination), formatRod(spare)))
+                .append(System.lineSeparator());
     }
 
     public static String formatRod(Deque<Integer> stack) {
