c14e18d88f3290d5e454bc5859147294db959a1f
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-06-10 15:02:09 +0300

    Add files via upload

--- Algorithms with Java/Fundamentals/Graph Theory, Traversal and Shortest Paths_Lab/src/main/java/Main.java
+++ Algorithms with Java/Fundamentals/Graph Theory, Traversal and Shortest Paths_Lab/src/main/java/Main.java
@@ -4,34 +4,35 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static boolean[] visited;
-    public static List<List<Integer>> graph = new ArrayList<>();
+    //    public static List<List<Integer>> graph = new ArrayList<>();
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 0; i < n; i++) {
-            String str = scanner.nextLine();
-
-            if (str.trim().equals("")) {
-                graph.add(new ArrayList<>());
-            } else {
-                List<Integer> inputLine = Arrays.stream(str.split("\\s+"))
-                        .map(Integer::parseInt).collect(Collectors.toList());
-                graph.add(inputLine);
-            }
-        }
-
-        List<Deque<Integer>> connectedComponents = getConnectedComponents(graph);
-
-        for (Deque<Integer> connectedComponent : connectedComponents) {
-            System.out.print("Connected component: ");
-            for (Integer integer : connectedComponent) {
-                System.out.print(integer + " ");
-            }
-            System.out.println();
-        }
+//        for (int i = 0; i < n; i++) {
+//            String str = scanner.nextLine();
+//
+//            if (str.trim().equals("")) {
+//                graph.add(new ArrayList<>());
+//            } else {
+//                List<Integer> inputLine = Arrays.stream(str.split("\\s+"))
+//                        .map(Integer::parseInt).collect(Collectors.toList());
+//                graph.add(inputLine);
+//            }
+//        }
+//
+//        List<Deque<Integer>> connectedComponents = getConnectedComponents(graph);
+//
+//        for (Deque<Integer> connectedComponent : connectedComponents) {
+//            System.out.print("Connected component: ");
+//            for (Integer intgr : connectedComponent) {
+//                System.out.print(intgr + " ");
+//            }
+//            System.out.println();
+//        }
     }
 
     private static void dfs(int vertex, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
@@ -60,7 +61,59 @@ public class Main {
         return components;
     }
 
+
     public static Collection<String> topSort(Map<String, List<String>> graph) {
-        throw new AssertionError("Not Implemented");
+        Map<String, Integer> dependenciesCount = getDependenciesCount(graph);
+
+        List<String> sorted = new ArrayList<>();
+
+        while (!graph.isEmpty()) {
+            String key = graph.keySet()
+                    .stream()
+                    .filter(k -> dependenciesCount.get(k) == 0)
+                    .findFirst()
+                    .orElse(null);
+
+            if (key == null) {
+                break;
+            }
+
+            for (String child : graph.get(key)) {
+                dependenciesCount.put(child, dependenciesCount.get(child) - 1);
+            }
+
+            sorted.add(key);
+            graph.remove(key);
+        }
+
+        if (!graph.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        return sorted;
+    }
+
+    private static Map<String, Integer> getDependenciesCount(Map<String, List<String>> graph) {
+        Map<String, Integer> dependenciesCount = new LinkedHashMap<>();
+
+        for (Map.Entry<String, List<String>> node : graph.entrySet()) {
+            dependenciesCount.putIfAbsent(node.getKey(), 0);
+            for (String child : node.getValue()) {
+                dependenciesCount.putIfAbsent(child, 0);
+                dependenciesCount.put(child, dependenciesCount.get(child) + 1);
+            }
+        }
+
+        return dependenciesCount;
     }
 }
+
+
+
+
+
+
+
+
+
+
