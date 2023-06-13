f3e94e5c4feb34acf40bca107d68075166d59720
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-06-13 16:07:44 +0300

    Add files via upload

--- Algorithms with Java/Fundamentals/Graph Theory, Traversal and Shortest Paths_Exercise/src/E03_CyclesInGraph.java
+++ Algorithms with Java/Fundamentals/Graph Theory, Traversal and Shortest Paths_Exercise/src/E03_CyclesInGraph.java
@@ -1,2 +1,61 @@
+import java.util.*;
+
 public class E03_CyclesInGraph {
+
+    public static Map<String, List<String>> graph = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+        String source = null;
+
+        while (!line.equals("End")) {
+            String[] tokens = line.split("-");
+
+            if (source == null) {
+                source = tokens[0];
+            }
+
+            graph.putIfAbsent(tokens[0], new ArrayList<>());
+            graph.get(tokens[0]).add(tokens[1]);
+
+            line = scanner.nextLine();
+        }
+
+        Set<String> visited = new HashSet<>();
+        Set<String> cycles = new HashSet<>();
+
+        try {
+            dfs(source, visited, cycles);
+            System.out.println("Acyclic: Yes");
+        } catch (IllegalStateException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void dfs(String source, Set<String> visited, Set<String> cycles) {
+        if (cycles.contains(source)) {
+            throw new IllegalStateException("Acyclic: No");
+        }
+
+        if (visited.contains(source)) {
+            return;
+        }
+
+        visited.add(source);
+        cycles.add(source);
+
+        List<String> children = graph.get(source);
+
+        if (children == null) {
+            return;
+        }
+
+        for (String child : graph.get(source)) {
+            dfs(child, visited, cycles);
+        }
+
+        cycles.remove(source);
+    }
 }
