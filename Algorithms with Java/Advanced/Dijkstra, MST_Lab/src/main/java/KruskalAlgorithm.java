565b6b457491553cba7160ef434722d61ae385d2
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-07-13 22:25:04 +0300

    Add files via upload

--- Algorithms with Java/Advanced/Dijkstra, MST_Lab/src/main/java/KruskalAlgorithm.java
+++ Algorithms with Java/Advanced/Dijkstra, MST_Lab/src/main/java/KruskalAlgorithm.java
@@ -1,12 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class KruskalAlgorithm {
 
     public static List<Edge> kruskal(int numberOfVertices, List<Edge> edges) {
-        return null;
+//        PriorityQueue<Edge> orderedEdges = new PriorityQueue<>(edges);
+
+        Collections.sort(edges);
+
+        List<Edge> forest = new ArrayList<>();
+
+        int[] parents = new int[numberOfVertices];
+
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
+        }
+
+        while (!edges.isEmpty()) {
+            Edge edge = edges.remove(0);
+
+            int startNode = edge.getStartNode();
+            int endNode = edge.getEndNode();
+
+            int firstParent = findRoot(startNode, parents);
+            int secondParent = findRoot(endNode, parents);
+
+            if (firstParent != secondParent) {
+                forest.add(edge);
+                parents[firstParent] = secondParent;
+            }
+
+        }
+
+        return forest;
     }
 
     public static int findRoot(int node, int[] parents) {
-        return 0;
+
+        while (parents[node] != node) {
+            node = parents[node];
+        }
+
+        return node;
     }
 }
