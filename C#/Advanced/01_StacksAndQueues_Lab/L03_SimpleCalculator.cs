065b5ceb6c621d1fa0e70778873de5039f78fcc6
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-15 23:02:41 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Lab/L03_SimpleCalculator.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Queue<string> queue = new Queue<string>(Console.ReadLine().Split());
+
+int result = 0;
+
+
+
+while (queue.Count > 0)
+{
+    string element = queue.Dequeue();
+
+    if (element == "-")
+    {
+        result -= int.Parse(queue.Dequeue());
+    } 
+    else if (element == "+")
+    {
+        result += int.Parse(queue.Dequeue());
+    }
+    else
+    {
+        result += int.Parse(element);
+    }
+}
+
+Console.WriteLine(result);
