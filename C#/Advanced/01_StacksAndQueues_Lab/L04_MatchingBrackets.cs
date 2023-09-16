dbd637e4c0cdd30e24ce928f957840379277621d
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-16 07:49:55 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Lab/L04_MatchingBrackets.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+string expression = Console.ReadLine();
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < expression.Length; i++)
+{
+
+    if (expression[i] == '(') 
+    {
+        stack.Push(i);
+    }
+
+    if (expression[i] == ')') 
+    { 
+        int openBracketsIndex = stack.Pop();
+
+        Console.WriteLine(expression.Substring(openBracketsIndex, i - openBracketsIndex +1));
+    }
+    
+}
