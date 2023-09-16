dfdbb1a90e0b88017e0cb9f38b995f7dc74cf924
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-16 09:38:14 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Lab/L08_TrafficJam.cs
@@ -0,0 +1,33 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Queue<string> cars = new Queue<string>();
+
+int passedCars = 0;
+
+string command = Console.ReadLine();
+
+while(command != "end")
+{
+    if(command == "green")
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if(cars.Count == 0)
+            {
+                break;
+            }
+
+            Console.WriteLine($"{cars.Dequeue()} passed!");
+            passedCars++;
+        }
+
+    }
+    else
+    {
+        cars.Enqueue(command);
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine($"{passedCars} cars passed the crossroads.");
