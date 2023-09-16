a5350657ee783850198d795cb0e302fc5a71b361
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-16 09:06:09 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Lab/L06_Supermarket.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Queue<string> customers = new Queue<string> ();
+
+string command = Console.ReadLine();
+
+while(command!="End") 
+{
+    if(command != "Paid")
+    {
+        customers.Enqueue(command);
+    }
+    else if(command == "Paid")
+    {
+        while(customers.Count > 0) 
+        { 
+        Console.WriteLine (customers.Dequeue());
+        }
+    }
+    command = Console.ReadLine();
+}
+
+Console.WriteLine($"{customers.Count} people remaining.");
