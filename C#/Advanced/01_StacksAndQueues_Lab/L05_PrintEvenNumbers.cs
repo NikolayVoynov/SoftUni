2643f95b71901934c4fb51c5293df1429adbdfed
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-16 08:16:25 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Lab/L05_PrintEvenNumbers.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Queue<int> queue = new Queue<int>();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i]%2 == 0) 
+    {
+        queue.Enqueue(arr[i]);
+    } 
+}
+
+StringBuilder sb = new StringBuilder();
+
+while (queue.Count > 1)
+{
+    sb.Append(queue.Dequeue())
+        .Append(", ");
+}
+
+sb.Append(queue.Dequeue());
+
+Console.WriteLine(sb.ToString());
+
