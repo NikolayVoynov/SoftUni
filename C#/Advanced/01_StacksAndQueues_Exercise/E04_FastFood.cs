6d6e9cebee75db34db75c544d13381b6a8af9910
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-17 13:25:06 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Exercise/E04_FastFood.cs
@@ -0,0 +1,28 @@
+﻿int foodQuantity = int.Parse(Console.ReadLine());
+int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Queue<int> queueOrders = new Queue<int>(orders);
+
+while (queueOrders.Count > 0 && foodQuantity > 0)
+{
+    int currentOrder = queueOrders.Peek();
+
+    if (foodQuantity - currentOrder >= 0)
+    {
+        foodQuantity -= queueOrders.Dequeue();
+    }
+    else
+    {
+        break;
+    }
+}
+
+if (queueOrders.Count == 0)
+{
+    Console.WriteLine("Orders complete");
+}
+else
+{
+    Console.WriteLine($"Orders left: {string.Join(" ", queueOrders)}");
+}
+
+
