8c1bbe20f96a582f8e2addd2aa680b9ab2c52c62
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-17 11:29:29 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Exercise/E02_BasicQueueOperations.cs
@@ -0,0 +1,27 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int addInQueue = input[0];
+int removeFromQueue = input[1];
+int lookFor = input[2];
+
+Queue<int> queue = new Queue<int>(values.Take(addInQueue));
+
+while (queue.Count > 0 && removeFromQueue > 0)
+{
+    queue.Dequeue();
+    removeFromQueue--;
+}
+
+if (queue.Count == 0)
+{
+    Console.WriteLine(0);
+}
+else if (queue.Contains(lookFor))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine(queue.Min());
+}
