7adb1f90ac8e905fea1bb9480b7e4029dc89988e
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-17 15:21:41 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Exercise/Е06_SongsQueue.cs
@@ -0,0 +1,42 @@
+﻿string[] inputSongs = Console.ReadLine().Split(", ");
+
+Queue<string> songs = new Queue<string>(inputSongs);
+
+while (true)
+{
+    string[] command = Console.ReadLine().Split();
+
+    if (command[0] == "Play")
+    {
+        if(songs.Count > 0)
+        {
+            songs.Dequeue();
+        }
+
+        if (songs.Count == 0)
+        {
+            Console.WriteLine("No more songs!");
+            break;
+        }
+    }
+
+    if (command[0] == "Add")
+    {
+        string songName = string.Join(" ", command.Skip(1));
+
+        if (songs.Contains(songName))
+        {
+            Console.WriteLine($"{songName} is already contained!");
+        }
+        else
+        {
+            songs.Enqueue(songName);
+        }
+    }
+
+    if (command[0] == "Show")
+    {    
+        Console.WriteLine(string.Join(", ", songs));
+    }
+}
+        
