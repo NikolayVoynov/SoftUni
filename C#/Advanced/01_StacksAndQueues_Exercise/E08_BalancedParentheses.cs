4dd4a501c226a921a3e658cd2c976083ccecdadf
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-17 17:53:22 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Exercise/E08_BalancedParentheses.cs
@@ -0,0 +1,42 @@
+﻿string parentheses = Console.ReadLine();
+
+Stack<char> stackOfParentheses = new Stack<char>();
+
+foreach (char elem in parentheses)
+{
+    if (elem == '(' || elem == '[' || elem == '{')
+    {
+        stackOfParentheses.Push(elem);
+    }
+
+    if (stackOfParentheses.Count == 0)
+    {
+        stackOfParentheses.Push(elem);
+        break;
+    }
+
+    if (elem == ')' && stackOfParentheses.Peek() == '(' )
+    {
+        stackOfParentheses.Pop();
+    }
+
+    if (elem == ']' && stackOfParentheses.Peek() == '[')
+    {
+        stackOfParentheses.Pop();
+    }
+
+    if (elem == '}' && stackOfParentheses.Peek() == '{')
+    {
+        stackOfParentheses.Pop();
+    }
+}
+
+if(stackOfParentheses.Count == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
+
