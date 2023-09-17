f2486c3a125395a9b1dd21334e3944f039216dae
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-17 12:16:39 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Exercise/E03_MaximumAndMinimumElement.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < n; i++)
+{
+    int[] commandInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    int command = commandInfo[0];
+
+    if(command == 1)
+    {
+        stack.Push(commandInfo[1]);
+    }
+    else if(command == 2)
+    {
+        stack.Pop();
+    }
+    else if(command == 3)
+    {
+        Console.WriteLine(stack.Max());
+    }
+    else if (command == 4)
+    {
+        Console.WriteLine(stack.Min());
+    }
+}
+
+Console.WriteLine(string.Join(", ", stack));
