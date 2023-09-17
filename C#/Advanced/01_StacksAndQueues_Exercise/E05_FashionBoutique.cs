a3cfdb0bb3114bfc1ff89fd6ac0712cbcdcb1bfe
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-17 14:43:58 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Exercise/E05_FashionBoutique.cs
@@ -0,0 +1,23 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Stack<int> stack = new Stack<int>(input);
+
+int rackCapacity = int.Parse(Console.ReadLine());
+int usedRacks = 1;
+int currentCapacity = 0;
+
+while (stack.Count > 0)
+{
+    int currentValue = stack.Pop();
+    
+    if(currentValue + currentCapacity <= rackCapacity)
+    {
+        currentCapacity += currentValue;
+    }
+    else
+    {
+        usedRacks++;
+        currentCapacity = currentValue;
+    }
+}
+
+Console.WriteLine(usedRacks);
