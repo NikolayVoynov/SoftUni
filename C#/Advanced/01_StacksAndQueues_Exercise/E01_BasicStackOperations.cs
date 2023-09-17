8885532b46d562f362e184399ca14eeea9f5eacb
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-17 10:46:12 +0300

    Add files via upload

---
+++ C#/Advanced/01_StacksAndQueues_Exercise/E01_BasicStackOperations.cs
@@ -0,0 +1,27 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int pushInStack = input[0];
+int popFromStack = input[1];
+int lookFor = input[2];
+
+Stack<int> stack = new Stack<int>(values.Take(pushInStack));
+
+while (stack.Count > 0 && popFromStack > 0)
+{
+    stack.Pop();
+    popFromStack--;
+}
+
+if(stack.Count==0)
+{
+    Console.WriteLine(0);
+}
+else if(stack.Contains(lookFor))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine(stack.Min());
+}
