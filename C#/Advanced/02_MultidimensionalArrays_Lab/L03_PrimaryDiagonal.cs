30d99ca7575e1e44fa76136893eddb11cb76f53c
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-19 20:42:07 +0300

    Add files via upload

---
+++ C#/Advanced/02_MultidimensionalArrays_Lab/L03_PrimaryDiagonal.cs
@@ -0,0 +1,21 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[size,size];
+
+for (int row = 0; row < size; row++)
+{
+    int[] rowValues = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    for (int col = 0; col < size; col++)
+    {
+        matrix[row, col] = rowValues[col];
+    }
+}
+
+int sumDiagonal = 0;
+
+for (int i = 0; i < size; i++)
+{
+    sumDiagonal += matrix[i,i];
+}
+
+Console.WriteLine(sumDiagonal);
