a9091bc088dd94fb86d9fe6b9e15c60ca2a085d4
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-19 20:53:55 +0300

    Add files via upload

---
+++ C#/Advanced/02_MultidimensionalArrays_Lab/L06_JaggedArrayModification.cs
@@ -0,0 +1,49 @@
+﻿int rows = int.Parse(Console.ReadLine());
+
+int[][] matrix = new int[rows][];
+
+for (int r = 0; r < rows; r++)
+{
+    int[] rowValues = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    matrix[r] = rowValues;
+}
+
+string command = Console.ReadLine().ToLower();
+
+while (command != "end")
+{
+    string[] input = command.Split();
+    int row = int.Parse(input[1]);
+    int col = int.Parse(input[2]);
+    int value = int.Parse(input[3]);
+
+    if (row < 0 || col < 0 || row >= matrix.Length || col >= matrix[row].Length)
+    {
+        Console.WriteLine("Invalid coordinates");
+    }
+    else
+    {
+        if (input[0] == "add")
+        {
+            matrix[row][col] += value;
+        }
+
+        else
+        {
+            matrix[row][col] -= value;
+        }
+    }
+
+    command = Console.ReadLine().ToLower();
+}
+
+
+for (int r = 0; r < matrix.Length; r++)
+{
+    for (int c = 0; c < matrix[r].Length; c++)
+    {
+        Console.Write($"{matrix[r][c]} ");
+    }
+
+    Console.WriteLine();
+}
