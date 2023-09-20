26e39818c9ffaca8b52133d41f2e3949499235cc
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-20 09:18:51 +0300

    Add files via upload

---
+++ C#/Advanced/02_MultidimensionalArrays_Exercise/E05_SnakeMoves.cs
@@ -0,0 +1,58 @@
+﻿int[] dimensions = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+string word = Console.ReadLine();
+
+int rows = dimensions[0];
+int cols = dimensions[1];
+
+char[,] matrix = new char[rows, cols];
+
+int currentIndex = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    if (row % 2 == 0)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            if (currentIndex == word.Length)
+            {
+                currentIndex = 0;
+            }
+
+            matrix[row, col] = word[currentIndex];
+
+            currentIndex++;
+        }
+    }
+    else
+    {
+        for (int col = cols - 1; col >= 0; col--)
+        {
+            if (currentIndex == word.Length)
+            {
+                currentIndex = 0;
+            }
+
+            matrix[row, col] = word[currentIndex];
+
+            currentIndex++;
+        }
+    }
+}
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col<cols; col++)
+    {
+        Console.Write($"{matrix[row, col]}");
+    }
+
+    Console.WriteLine();
+}
+
+
+
