3a3dab229e57f2073370129491438261a4a49627
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-20 09:17:17 +0300

    Add files via upload

---
+++ C#/Advanced/02_MultidimensionalArrays_Exercise/E01_DiagonalDifference.cs
@@ -0,0 +1,27 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[n, n];
+
+for (int row = 0; row < n; row++)
+{
+    int[] numbers = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(n => int.Parse(n))
+        .ToArray();
+
+    for (int col = 0; col < n; col++)
+    {
+        matrix[row, col] = numbers[col];
+    }
+}
+
+int primaryDiagonal = 0;
+int secondaryDiagonal = 0;
+
+for (int i = 0; i < n; i++)
+{
+    primaryDiagonal += matrix[i, i];
+    secondaryDiagonal += matrix[i, n - 1 - i];
+}
+
+Console.WriteLine(Math.Abs(primaryDiagonal - secondaryDiagonal));
