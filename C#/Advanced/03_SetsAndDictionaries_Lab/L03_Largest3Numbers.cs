948fe880732d0668ff1d163c4bcf0cd1b91e3d88
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-21 18:55:03 +0300

    Add files via upload

---
+++ C#/Advanced/03_SetsAndDictionaries_Lab/L03_Largest3Numbers.cs
@@ -0,0 +1,8 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .OrderByDescending(n => n)
+    .Take(3)
+    .ToList();
+
+Console.WriteLine(String.Join(" ", numbers));
