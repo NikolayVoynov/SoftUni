e25ac33b9557268d0b1ae43dcc6c7e13aef0460e
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-21 23:21:59 +0300

    Add files via upload

---
+++ C#/Advanced/03_SetsAndDictionaries_Lab/L05_CitiesByContinentAndCountry.cs
@@ -0,0 +1,36 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<String, Dictionary<String, List<string>>> citiesByContinentAndCountries = new Dictionary<string, Dictionary<string, List<string>>>();
+
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string continent = input[0];
+    string country = input[1];
+    string city = input[2];
+
+    if (!citiesByContinentAndCountries.ContainsKey(continent))
+    {
+        citiesByContinentAndCountries[continent] = new Dictionary<string, List<string>>();
+    }
+    
+
+    if (!citiesByContinentAndCountries[continent].ContainsKey(country))
+    {
+        citiesByContinentAndCountries[continent][country] = new List<string>();
+    }
+
+    citiesByContinentAndCountries[continent][country].Add(city);
+}
+
+foreach(var entity in citiesByContinentAndCountries)
+{
+    Console.WriteLine($"{entity.Key}:");
+
+    foreach (var countryCity in entity.Value)
+    {
+        Console.WriteLine($" {countryCity.Key} -> {String.Join(", ", countryCity.Value)}");
+    }
+}
