b0916dd1c74ca275b760df133ef19702831658a7
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-21 18:53:59 +0300

    Add files via upload

---
+++ C#/Advanced/03_SetsAndDictionaries_Lab/L01_CountSameValuesInArray.cs
@@ -0,0 +1,24 @@
+﻿List<double> numbers = Console.ReadLine()
+                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                       .Select(double.Parse)
+                       .ToList();
+
+Dictionary<double, int> countOccurrences = new Dictionary<double, int>();
+
+foreach (double number in numbers)
+{
+    if(!countOccurrences.ContainsKey(number))
+    {
+        countOccurrences.Add(number, 1);
+    }
+    else
+    {
+        countOccurrences[number]++;
+    }
+}
+
+
+foreach (var entry in countOccurrences)
+{
+    Console.WriteLine($"{entry.Key} - {entry.Value} times");
+}
