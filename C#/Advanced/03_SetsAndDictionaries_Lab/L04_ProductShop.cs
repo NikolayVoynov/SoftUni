e46aad47e55b5133a5329de5d6331d26dee7899f
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-21 18:55:28 +0300

    Add files via upload

---
+++ C#/Advanced/03_SetsAndDictionaries_Lab/L04_ProductShop.cs
@@ -0,0 +1,31 @@
+﻿SortedDictionary<string, Dictionary<string, double>> productShop = new SortedDictionary<string, Dictionary<string, double>>(); 
+
+string command = Console.ReadLine();
+
+while(command != "Revision")
+{
+    string[] input = command.Split(", ");
+    string shop = input[0];
+    string product = input[1];
+    double price = double.Parse(input[2]);
+
+    if (!productShop.ContainsKey(shop))
+    {
+        productShop.Add(shop, new Dictionary<string, double>());
+    }
+
+    productShop[shop].Add(product, price);
+
+    command = Console.ReadLine();
+}
+
+
+foreach(var entry in productShop)
+{
+    Console.WriteLine($"{entry.Key}->");
+
+    foreach(var product in entry.Value)
+    {
+        Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+    }
+}
