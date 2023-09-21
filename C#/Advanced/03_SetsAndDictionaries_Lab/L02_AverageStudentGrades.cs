c8ed4a21fb2c51e29eeb4e5bc28598af60c404af
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-21 18:54:26 +0300

    Add files via upload

---
+++ C#/Advanced/03_SetsAndDictionaries_Lab/L02_AverageStudentGrades.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<decimal>> studentsAndGrades = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] nameGrade = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    string name = nameGrade[0];
+    decimal grade = decimal.Parse(nameGrade[1]);
+
+    if (!studentsAndGrades.ContainsKey(name))
+    {
+        studentsAndGrades.Add(name, new List<decimal>());
+    }
+
+    studentsAndGrades[name].Add(grade);
+}
+
+foreach (var entry in studentsAndGrades)
+{
+    string name = entry.Key;
+    List<decimal> grades = entry.Value;
+    decimal avrgGrade = entry.Value.Average();
+
+    Console.WriteLine($"{name} -> {String.Join(" ", grades.Select(grade => $"{grade:F2}"))} (avg: {avrgGrade:F2})");
+}
+
+
