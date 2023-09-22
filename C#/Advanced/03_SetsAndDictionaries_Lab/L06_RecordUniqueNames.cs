7e23d5c44d22090e39557cd3239a8ce5a5688084
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-22 20:46:50 +0300

    Add files via upload

---
+++ C#/Advanced/03_SetsAndDictionaries_Lab/L06_RecordUniqueNames.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+HashSet<string> result = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string name = Console.ReadLine();
+
+    result.Add(name);
+}
+
+
+foreach (string name in result)
+{
+    Console.WriteLine(name);
+}
