27cae3fe47eb00b2b99fd5fbb5cfc7dfa5a76c7e
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-24 09:25:11 +0300

    Add files via upload

---
+++ C#/Advanced/03_SetsAndDictionaries_Exercise/E02_SetsOfElements.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+int[] input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int n = input[0];
+int m = input[1];
+
+HashSet<int> first = new HashSet<int>();
+HashSet<int> second = new HashSet<int>();
+
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    first.Add(number);
+}
+
+for (int i = 0; i < m; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    second.Add(number);
+}
+
+first.IntersectWith(second);
+
+Console.WriteLine(string.Join(" ", first));
+
