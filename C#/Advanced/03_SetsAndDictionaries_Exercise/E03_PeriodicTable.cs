2ed0e2b50b298703d55fb332aba956dad08d3af0
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-25 07:05:10 +0300

    Add files via upload

---
+++ C#/Advanced/03_SetsAndDictionaries_Exercise/E03_PeriodicTable.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+SortedSet<string> elements = new SortedSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] inputArr = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    elements.UnionWith(inputArr);
+}
+
+Console.WriteLine(String.Join(" ", elements));
+
---
+++ C#/Advanced/03_SetsAndDictionaries_Exercise/E04_EvenTimes.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<int, int> numAppear = new Dictionary<int, int>();
+
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (!numAppear.ContainsKey(number))
+    {
+        numAppear.Add(number, 0);
+    }
+
+    numAppear[number]++;
+}
+
+foreach (var entry in numAppear)
+{
+    if (entry.Value % 2 == 0)
+    {
+        Console.WriteLine(entry.Key);
+    }
+}
+
