5f0acf0383c95986b7a288bd99c70e3137c54ea7
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-09-25 07:03:56 +0300

    Add files via upload

---
+++ C#/Advanced/03_SetsAndDictionaries_Exercise/Е05_CountSymbols.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+string word = Console.ReadLine();
+
+SortedDictionary<char, int> letterOccurrences = new SortedDictionary<char, int>();
+
+foreach (var letter in word)
+{
+    if (!letterOccurrences.ContainsKey(letter))
+    {
+        letterOccurrences.Add(letter, 0);
+    }
+
+    letterOccurrences[letter]++;
+}
+
+foreach (var entry in letterOccurrences)
+{
+    Console.WriteLine($"{entry.Key}: {entry.Value} time/s");
+}
+
