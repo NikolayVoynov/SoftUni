8b9ff4f8e88624cc21dc9cdc3c69f96bbff77e23
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-10-17 22:49:05 +0300

    Add files via upload

---
+++ C#/Advanced/05_FunctionalProgramming_Lab/L01_SortEvenNumbers/L01_SortEvenNumbers.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(new string[] { ", " },
+    StringSplitOptions.RemoveEmptyEntries)
+    .Select(n => int.Parse(n))
+    .Where(n => n % 2 == 0)
+    .OrderBy(n => n)
+    .ToArray();
+
+string result = string.Join(", ", numbers);
+
+Console.WriteLine(result);
