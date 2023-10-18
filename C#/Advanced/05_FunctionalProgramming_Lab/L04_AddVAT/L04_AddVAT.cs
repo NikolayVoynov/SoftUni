16e080a6d6b4ab7a6ed3cea9caced59fe8834910
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-10-18 07:58:38 +0300

    Add files via upload

---
+++ C#/Advanced/05_FunctionalProgramming_Lab/L04_AddVAT/L04_AddVAT.cs
@@ -0,0 +1,12 @@
+﻿double[] pricesVAT = Console.ReadLine()
+    .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(n => double.Parse(n))
+    .Select(n => n * 1.2)
+    .ToArray();
+
+
+foreach (var price in pricesVAT)
+{
+    Console.WriteLine(price);
+}
+
