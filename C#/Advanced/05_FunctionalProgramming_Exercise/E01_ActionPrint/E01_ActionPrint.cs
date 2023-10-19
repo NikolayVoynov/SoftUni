741a477e2a7a676e7a4ef9213585a4b458b842e9
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-10-19 08:00:19 +0300

    Add files via upload

---
+++ C#/Advanced/05_FunctionalProgramming_Exercise/E01_ActionPrint/E01_ActionPrint.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Action<string[]> namesPrint = names =>
+Console.WriteLine(string.Join(Environment.NewLine, names));
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+namesPrint(names);
