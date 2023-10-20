ccb7afe48d1367c98a44308b6562154f3f1765e6
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-10-20 09:00:11 +0300

    Add files via upload

---
+++ C#/Advanced/05_FunctionalProgramming_Exercise/E02_KnightsOfHonor/E02_KnightsOfHonor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Action<string, string[]> printNamesAndTitles = (title, names) =>
+{
+    foreach (var name in names)
+    {
+        Console.WriteLine($"{title} {name}");
+    }
+};
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+printNamesAndTitles("Sir", names);
