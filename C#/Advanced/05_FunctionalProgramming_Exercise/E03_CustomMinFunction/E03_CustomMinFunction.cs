26f9b80b50ea0febe0e793ed2f3a8957361e865c
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-10-20 09:00:59 +0300

    Add files via upload

---
+++ C#/Advanced/05_FunctionalProgramming_Exercise/E03_CustomMinFunction/E03_CustomMinFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+Func<HashSet<int>, int> min = numbers =>
+{
+    int minNum = int.MaxValue;
+
+    foreach (int num in numbers)
+    {
+        if (num < minNum) 
+        { 
+          minNum = num;
+        }
+    }
+
+    return minNum;
+};
+
+HashSet<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToHashSet();
+
+Console.WriteLine(min(numbers));
