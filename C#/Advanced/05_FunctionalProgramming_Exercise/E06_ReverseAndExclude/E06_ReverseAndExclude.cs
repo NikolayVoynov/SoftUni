86e4391e15ff96c95bc0ef93abdebdafd227ab45
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-10-22 19:28:53 +0300

    Add files via upload

---
+++ C#/Advanced/05_FunctionalProgramming_Exercise/E06_ReverseAndExclude/E06_ReverseAndExclude.cs
@@ -0,0 +1,43 @@
+﻿Func<List<int>, List<int>> reverse = numbers =>
+{
+    List<int> result = new List<int>();
+
+    for (int i = numbers.Count -1; i >= 0; i--)
+    {
+        result.Add(numbers[i]);
+    }
+
+    return result;
+};
+
+Func<List<int>, Predicate<int>, List<int>> devideAndExclude = (numbers, predicate) =>
+{
+    List<int> result = new List<int>();
+
+    foreach (int num in numbers)
+    {
+        if (!predicate(num))
+        {
+            result.Add(num);
+        }
+    }
+
+    return result;
+};
+
+List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+int divider = int.Parse(Console.ReadLine());
+
+Predicate<int> predicate = num =>
+num % divider == 0;
+
+numbers = devideAndExclude(numbers, predicate);
+numbers = reverse(numbers);
+
+Console.WriteLine(string.Join(" ", numbers));
+
+
