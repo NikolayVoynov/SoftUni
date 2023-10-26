67eb28f3f5cb092fcfc466605c92d5ffc9650a16
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-10-26 08:19:20 +0300

    Add files via upload

---
+++ C#/Advanced/06_DefiningClasses_Exercise/E10_SoftUniParking/Parking.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private int capacity;
+        private Dictionary<string, Car> cars;
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            cars = new Dictionary<string, Car>();
+        }
+
+        public int Count
+        {
+            get { return this.cars.Count; }
+        }
+
+        public string AddCar(Car car)
+        {
+            if (cars.ContainsKey(car.RegistrationNumber))
+            {
+                Console.WriteLine("Car with that registration number, already exists!");
+            }
+            else if(Count == capacity)
+            {
+                Console.WriteLine("Parking is full!");
+            }
+
+            cars.Add(car.RegistrationNumber, car);
+
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return cars[registrationNumber];
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if(!cars.ContainsKey(registrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+            cars.Remove(registrationNumber);
+
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string registrationNumber in registrationNumbers)
+            {
+                if(cars.ContainsKey((registrationNumber)))
+                {
+                    RemoveCar(registrationNumber);
+                }
+            }
+        }
+    }
+}
