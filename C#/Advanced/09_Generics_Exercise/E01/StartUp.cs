75c018b45f2f14dd7601acce637d9ae62fd4c804
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-10-29 07:28:38 +0200

    Add files via upload

---
+++ C#/Advanced/09_Generics_Exercise/E01/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfString
+{
+    public class StartUp
+    {
+
+        static void Main(string[] args)
+        {
+            Box<string> box = new();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string item = Console.ReadLine();
+
+                box.Add(item);
+            }
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
---
+++ C#/Advanced/09_Generics_Exercise/E02_GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int item = int.Parse(Console.ReadLine());
+
+                box.Add(item);
+            }
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
