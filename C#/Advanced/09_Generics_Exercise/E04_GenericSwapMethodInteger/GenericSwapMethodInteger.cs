51bb93ee7651a970a75eb55374877fbfb548e19c
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-10-30 08:00:47 +0200

    Add files via upload

---
+++ C#/Advanced/09_Generics_Exercise/E04_GenericSwapMethodInteger/GenericSwapMethodInteger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<int> items = new();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    int item = int.Parse(Console.ReadLine());
+
+    items.Add(item);
+}
+
+int[] indices = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Swap(indices[0], indices[1], items);
+
+static void Swap<T>(int firstIndex, int secondIndex, List<T> items)
+{
+    (items[firstIndex], items[secondIndex]) = (items[secondIndex], items[firstIndex]);
+}
+
+foreach (int item in items)
+{
+    Console.WriteLine($"{typeof(int)}: {item}");
+}
+
