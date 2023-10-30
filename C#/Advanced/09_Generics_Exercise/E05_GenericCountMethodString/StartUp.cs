3a568d9519600378991377c699ee292e3405517b
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-10-30 08:02:05 +0200

    Add files via upload

---
+++ C#/Advanced/09_Generics_Exercise/E05_GenericCountMethodString/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string item = Console.ReadLine();
+
+                box.Add(item);
+            }
+
+            string itemToCompare = Console.ReadLine();
+
+            Console.WriteLine(box.Count(itemToCompare));
+        }
+    }
+}
