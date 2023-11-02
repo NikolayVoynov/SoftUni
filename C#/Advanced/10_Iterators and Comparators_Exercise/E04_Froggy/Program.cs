c680adfa93853b483af2661b4fcca405560d9e7c
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-11-02 14:24:02 +0200

    Add files via upload

---
+++ C#/Advanced/10_Iterators and Comparators_Exercise/E04_Froggy/Program.cs
@@ -0,0 +1,13 @@
+﻿using Froggy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<int> stones = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+Lake lake = new Lake(stones);
+
+Console.WriteLine(String.Join(", ",lake));
