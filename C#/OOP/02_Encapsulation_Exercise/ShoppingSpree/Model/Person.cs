5fdf9b2fe596f56be7f3998ef0ac0c1a22145627
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-11-07 15:42:40 +0200

    Add files via upload

---
+++ C#/OOP/02_Encapsulation_Exercise/ShoppingSpree/Model/Person.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bag;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            bag = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public string Add(Product product)
+        {
+            if (Money < product.Cost)
+            {
+                return $"{Name} can't afford {product.Name}";
+            }
+
+            bag.Add(product);
+
+            Money -= product.Cost;
+
+            return $"{Name} bought {product.Name}";
+        }
+
+        public override string ToString()
+        {
+            string productsString = bag.Any()
+                ? string.Join(", ", bag.Select(p => p.Name))
+                : "Nothing bought";
+
+            return $"{Name} - {productsString}";
+        }
+    }
+}
