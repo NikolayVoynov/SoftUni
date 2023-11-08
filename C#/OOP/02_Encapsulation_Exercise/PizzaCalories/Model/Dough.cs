2f34ff5d61a4d3aa814833dede040f95e1691be2
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-11-08 10:17:23 +0200

    Add files via upload

---
+++ C#/OOP/02_Encapsulation_Exercise/PizzaCalories/Model/Dough.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaCalories.Model
+{
+    public class Dough
+    {
+        private const double BaseCaloriesPerGram = 2;
+
+        Dictionary<string, double> flourTypesCalories;
+        Dictionary<string, double> bakingTechniquesCalories;
+
+        private string flourType;
+        private string bakingTechnique;
+        private double weight;
+
+        public Dough(string flourType, string bakingTechnique, double weight)
+        {
+            flourTypesCalories = new Dictionary<string, double>
+            { { "white", 1.5 }, { "wholegrain", 1.0 } };
+
+            bakingTechniquesCalories = new Dictionary<string, double> 
+            { { "crispy", 0.9 }, { "chewy", 1.1 }, {"homemade", 1.0 } };
+
+            FlourType = flourType;
+            BakingTechnique = bakingTechnique;
+            Weight = weight;
+        }
+
+        public string FlourType
+        {
+            get { return this.flourType; }
+            private set 
+            { 
+                if(!flourTypesCalories.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+                
+                this.flourType = value.ToLower(); 
+            }
+        }
+
+        public string BakingTechnique
+        {
+            get { return this.bakingTechnique; }
+            private set
+            {
+                if (!bakingTechniquesCalories.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+
+                this.bakingTechnique = value.ToLower();
+            }
+        }
+
+        public double Weight
+        {
+            get { return this.weight; }
+            private set 
+            {
+                if (value < 1 || value > 200)
+                {
+                    throw new ArgumentException("Dough weight should be in the range [1..200].");
+                }
+
+                this.weight = value; 
+            }
+        }
+
+        public double Calories 
+        { 
+            get
+            {
+                double flourTypeModifier = flourTypesCalories[FlourType];
+                double bakingTechniqueModifier = bakingTechniquesCalories[BakingTechnique];
+
+                return BaseCaloriesPerGram * Weight * flourTypeModifier * bakingTechniqueModifier;
+            }
+        }
+    }
+}
---
+++ C#/OOP/02_Encapsulation_Exercise/PizzaCalories/StartUp.cs
@@ -0,0 +1,35 @@
+﻿
+using PizzaCalories.Model;
+using System;
+
+try
+{
+    string[] pizzaToken = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string[] doughToken = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    Dough dough = new Dough(doughToken[1], doughToken[2], double.Parse(doughToken[3]));
+
+    Pizza pizza = new Pizza(pizzaToken[1], dough);
+
+    string input = Console.ReadLine();
+
+    while (input != "END")
+    {
+        string[] toppingToken = input.Split(" ");
+
+        Topping topping = new Topping(toppingToken[1], double.Parse(toppingToken[2]));
+
+        pizza.AddTopping(topping);
+
+        input = Console.ReadLine();
+    }
+
+    Console.WriteLine(pizza.ToString());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
