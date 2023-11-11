e476a3a866575b8d185e939d674738c45b81acf0
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-11-11 18:06:23 +0200

    Add files via upload

---
+++ C#/OOP/03_InterfacesAbstraction_Exercise/E06_FoodShortage/Model/Citizen.cs
@@ -0,0 +1,38 @@
+﻿using FoodShortage.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodShortage.Model
+{
+    public class Citizen : IIdentifiable, INameable, IBirthable, IBuyer
+    {
+       
+
+        public Citizen(string name, int age, string id, string birthday)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthday = birthday;
+            Food = 0;
+        }
+
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Birthday { get; private set; }
+
+        public int Age { get; private set; }
+
+        public int Food { get; private set; }
+
+        public void BuyFood()
+        {
+            Food += 10;
+        }
+    }
+}
---
+++ C#/OOP/03_InterfacesAbstraction_Exercise/E06_FoodShortage/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using FoodShortage.Model;
+using FoodShortage.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FoodShortage;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<IBuyer> buyers = new();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] token = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (token.Length == 4)
+            {
+                IBuyer citizen = new Citizen(token[0], int.Parse(token[1]), token[2], token[3]);
+                buyers.Add(citizen);
+            }
+            else
+            {
+                IBuyer rebel = new Rebel(token[0], int.Parse(token[1]), token[2]);
+                buyers.Add(rebel);
+            }
+        }
+
+        string line = Console.ReadLine();
+
+        while (line != "End")
+        {
+            buyers.FirstOrDefault(b => b.Name == line)?.BuyFood();
+
+            line = Console.ReadLine();
+        }
+
+        Console.WriteLine(buyers.Sum(b => b.Food));
+    }
+}
