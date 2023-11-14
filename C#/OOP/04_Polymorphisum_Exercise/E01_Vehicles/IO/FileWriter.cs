cc7ff488d7da66231011b79b9e5f78fab1ddd86d
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-11-14 16:59:31 +0200

    Add files via upload

---
+++ C#/OOP/04_Polymorphisum_Exercise/E01_Vehicles/IO/FileWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles.IO.Interfaces;
+
+namespace Vehicles.IO
+{
+    public class FileWriter : IWriter
+    {
+        public void WriteLine(string str)
+        {
+            // using StreamWriter writer = new("");
+
+           // writer.WriteLine(str);
+        }
+    }
+}
