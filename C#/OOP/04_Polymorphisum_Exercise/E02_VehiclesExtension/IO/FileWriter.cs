26a558646932f89951cd05b12e4fe09229eb40ad
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-11-15 14:21:02 +0200

    Add files via upload

---
+++ C#/OOP/04_Polymorphisum_Exercise/E02_VehiclesExtension/IO/FileWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehiclesExtension.IO.Interfaces;
+
+namespace VehiclesExtension.IO
+{
+    public class FileWriter : IWriter
+    {
+        public void WriteLine(string str)
+        {
+            // using StreamWriter writer = new("");
+
+           // writer.WriteLine(str);
+        }
+    }
+}
---
+++ C#/OOP/04_Polymorphisum_Exercise/E02_VehiclesExtension/StartUp.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Vehicles.Factories.Interfaces;
+using VehiclesExtension.Core;
+using VehiclesExtension.Core.Interfaces;
+using VehiclesExtension.Factories;
+using VehiclesExtension.IO;
+using VehiclesExtension.IO.Interfaces;
+
+public class StartUp
+{
+
+    static void Main(string[] args)
+    {
+
+        IReader reader = new ConsoleReader();
+        IWriter writer = new ConsoleWriter();
+        IVehicleFactory vehicleFactory = new VehicleFactory();
+
+        IEngine engine = new Engine(reader, writer, vehicleFactory);
+
+        engine.Run();
+
+    }
+}
+
+
