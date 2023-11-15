a34fd9ba767c810d92244c4fcf7f8425384ba5e4
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-11-15 18:08:42 +0200

    Add files via upload

---
+++ C#/OOP/04_Polymorphisum_Exercise/E03_Raiding/Core/Engine.cs
@@ -0,0 +1,69 @@
+﻿using Raiding.Core.Interfaces;
+using Raiding.Factories.Interfaces;
+using Raiding.IO.Interfaces;
+using Raiding.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly IReader reader;
+        private readonly IWriter writer;
+        private readonly IHeroFactory heroFactory;
+
+        private readonly ICollection<IHero> heroes;
+
+        public Engine(IReader reader, IWriter writer, IHeroFactory heroFactory)
+        {
+            this.reader = reader;
+            this.writer = writer;
+            this.heroFactory = heroFactory;
+
+            heroes = new List<IHero>();
+        }
+        public void Run()
+        {
+            int count = int.Parse(reader.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = Console.ReadLine();
+                string type = Console.ReadLine();
+
+                try
+                {
+                    heroes.Add(heroFactory.Create(name, type));
+                }
+                catch (ArgumentException ex)
+                {
+                    writer.WriteLine(ex.Message);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            int bossPower = int.Parse(reader.ReadLine());
+
+            foreach (var hero in heroes)
+            {
+                writer.WriteLine(hero.CastAbility());
+            }
+
+            if (heroes.Sum(h => h.Power) > bossPower)
+            {
+                writer.WriteLine("Victory!");
+            }
+            else
+            {
+                writer.WriteLine("Defeat...");
+            }
+        }
+    }
+}
