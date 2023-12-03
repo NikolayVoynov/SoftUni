aad00107d3d07fcc212f971668647e2f514cdaf1
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-12-03 21:58:00 +0200

    Add files via upload

---
+++ C#/Advanced/ExamPrep_09Aug2023/VendingSystem/VendingSystem/Drink.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingSystem
+{
+    public class Drink
+    {
+        public Drink(string name, decimal price, int volume)
+        {
+            Name = name;
+            Price = price;
+            Volume = volume;
+        }
+
+        public string Name {  get; private set; }
+        public decimal Price {  get; private set; }
+        public int Volume {  get; private set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: ${Price}, Volume: {Volume} ml";
+        }
+    }
+}
---
+++ C#/Advanced/ExamPrep_09Aug2023/VendingSystem/VendingSystem/VendingMachine.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VendingSystem
+{
+    public class VendingMachine
+    {
+        public VendingMachine(int buttonCapacity)
+        {
+            ButtonCapacity = buttonCapacity;
+            Drinks = new List<Drink>();
+        }
+
+        public int ButtonCapacity { get; private set; }
+        public List<Drink> Drinks { get; private set; }
+
+        public int GetCount
+        {
+            get { return Drinks.Count; }
+        }
+
+        public void AddDrink(Drink drink)
+        {
+            if (GetCount < ButtonCapacity)
+            {
+                Drinks.Add(drink);
+            }
+        }
+
+        public bool RemoveDrink(string name)
+        {
+            return Drinks.Remove(Drinks.FirstOrDefault(drink => drink.Name == name));
+        }
+
+        public Drink GetLongest()
+        {
+            return Drinks.MaxBy(drink => drink.Volume);
+        }
+
+        public Drink GetCheapest()
+        {
+            return Drinks.MinBy(drink => drink.Price);
+        }
+
+        public string BuyDrink(string name)
+        {
+            return Drinks.FirstOrDefault(drink => drink.Name == name).ToString();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Drinks available:");
+
+            foreach(Drink drink in Drinks)
+            {
+                sb.AppendLine(drink.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
