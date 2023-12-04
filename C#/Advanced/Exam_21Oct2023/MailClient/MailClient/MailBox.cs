5c33bb231e7fdf27facafd75595a11d02058fe7f
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-12-04 22:18:40 +0200

    Add files via upload

---
+++ C#/Advanced/Exam_21Oct2023/MailClient/MailClient/MailBox.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace MailClient
+{
+    public class MailBox
+    {
+        public MailBox(int capacity)
+        {
+            Capacity = capacity;
+            Inbox = new List<Mail>();
+            Archive = new List<Mail>();
+        }
+
+        public int Capacity { get; set; }
+        public List<Mail> Inbox { get; set; }
+        public List<Mail> Archive { get; set; }
+
+        public void IncomingMail(Mail mail)
+        {
+            if (Inbox.Count() < Capacity)
+            {
+                Inbox.Add(mail);
+            }
+        }
+
+        public bool DeleteMail(string sender)
+        {
+            return Inbox.Remove(Inbox.FirstOrDefault(mail => mail.Sender == sender));
+        }
+
+        public int ArchiveInboxMessages()
+        {
+            int inboxMailsCount = Inbox.Count();
+
+            foreach (Mail mail in Inbox)
+            {
+                Archive.Add(mail);
+            }
+
+            Inbox.Clear();
+
+            return inboxMailsCount;
+        }
+
+        public string GetLongestMessage()
+        {
+            return Inbox.MaxBy(mail => mail.Body).ToString();
+        }
+
+        public string InboxView()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Inbox:");
+
+            foreach (Mail mail in Inbox) 
+            {
+                sb.AppendLine(mail.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#/Advanced/Exam_21Oct2023/OffroadChallenge/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+Stack<int> initialFuel = new Stack<int>(Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+Queue<int> additionalConsumption = new Queue<int>(Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+Queue<int> amountFuelNeeded = new Queue<int>(Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+List<string> altitudes = new List<string>();
+int altitude = 0;
+bool proceed = true;
+
+while (amountFuelNeeded.Any() && proceed)
+{
+    int fuel = initialFuel.Pop();
+    int consumption = additionalConsumption.Dequeue();
+
+    int result = fuel - consumption;
+
+    int neededFuel = amountFuelNeeded.Dequeue();
+
+    if (result < neededFuel)
+    {
+        altitude++;
+
+        Console.WriteLine($"John did not reach: Altitude {altitude}");
+        proceed = false;
+    }
+    else if (result >= neededFuel)
+    {
+        altitude++;
+        altitudes.Add($"Altitude {altitude}");
+        Console.WriteLine($"John has reached: Altitude {altitude}");
+    }
+}
+
+StringBuilder sb = new StringBuilder();
+
+if (amountFuelNeeded.Count == 0)
+{
+    sb.AppendLine("John has reached all the altitudes and managed to reach the top!");
+}
+else if (amountFuelNeeded.Count > 0 && altitudes.Count > 0)
+{
+    sb.AppendLine("John failed to reach the top.");
+    sb.AppendLine($"Reached altitudes: {String.Join(", ", altitudes)}");
+}
+else
+{
+    sb.AppendLine("John failed to reach the top.");
+    sb.AppendLine($"John didn't reach any altitude.");
+}
+
+Console.WriteLine(sb.ToString());
