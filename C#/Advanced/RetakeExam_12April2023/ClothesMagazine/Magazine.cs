743a0bc2eba907ebb294bda0c03c3ecc1344ae46
Nikolay Voynov <77551665+NikolayVoynov@users.noreply.github.com>
2023-12-05 23:22:07 +0200

    Add files via upload

---
+++ C#/Advanced/RetakeExam_12April2023/ClothesMagazine/Magazine.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClothesMagazine
+{
+    public class Magazine
+    {
+        public Magazine(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            Clothes = new List<Cloth>();
+        }
+
+        public string Type { get; private set; }
+        public int Capacity { get; private set; }
+        public List<Cloth> Clothes { get; private set; }
+
+        public void AddCloth(Cloth cloth)
+        {
+            if (Clothes.Count < Capacity)
+            {
+                Clothes.Add(cloth);
+            }
+        }
+
+        public bool RemoveCloth(string color)
+        {
+            return Clothes.Remove(Clothes.FirstOrDefault(clth => clth.Color == color));
+        }
+
+        public Cloth GetSmallestCloth()
+        {
+            return Clothes.MinBy(clth => clth.Size);
+        }
+
+        public Cloth GetCloth(string color)
+        {
+            return Clothes.FirstOrDefault(clth=> clth.Color == color);
+        }
+
+        public int GetClothCount()
+        {
+            return Clothes.Count;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{Type} magazine contains:");
+
+            sb.AppendLine(string.Join("\n",Clothes.OrderBy(clth=>clth.Size)));
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#/Advanced/RetakeExam_12April2023/RubberDuckDebugers/RubberDuckDebugers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Queue<int> times = new Queue<int>(Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+Stack<int> tasks = new Stack<int>(Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+Dictionary<string, int> numberDucksByType = new Dictionary<string, int>()
+{
+    {"Darth Vader Ducky", 0 },
+    {"Thor Ducky", 0 },
+    {"Big Blue Rubber Ducky", 0 },
+    {"Small Yellow Rubber Ducky", 0 }
+};
+
+while (times.Any() && tasks.Any())
+{
+    int result = times.Peek() * tasks.Peek();
+
+    if ((result >= 0) && (result <= 240))
+    {
+        if ((result >= 0) && (result <= 60))
+        {
+            numberDucksByType["Darth Vader Ducky"]++;
+        }
+        else if ((result >= 61) && (result <= 120))
+        {
+            numberDucksByType["Thor Ducky"]++;
+        }
+        else if ((result >= 121) && (result <= 180))
+        {
+            numberDucksByType["Big Blue Rubber Ducky"]++;
+        }
+        else if ((result >= 181) && (result <= 240))
+        {
+            numberDucksByType["Small Yellow Rubber Ducky"]++;
+        }
+
+        times.Dequeue();
+        tasks.Pop();
+    }
+    else
+    {
+        int task = tasks.Pop() - 2;
+        tasks.Push(task);
+
+        int time = times.Dequeue();
+        times.Enqueue(time);
+    }
+}
+
+Console.WriteLine("Congratulations, all tasks have been completed! Rubber ducks rewarded:");
+foreach (var duck in numberDucksByType)
+{
+    Console.WriteLine($"{duck.Key}: {duck.Value}");
+}
+
